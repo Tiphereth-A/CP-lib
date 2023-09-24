2d2b07e453c85f1609295ed80876cfd8cab4a6e8
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-09-24 23:10:28 +0800

    fix(inverse_matrix.test.cpp): tiny change

--- src/code/math/mat_inv.hpp
+++ src/code/math/mat_inv.hpp
@@ -13,7 +13,7 @@ inline std::optional<matrix<T>> inverse(matrix<T> const &mat, Ge ge) {
   assert(mat.row_size() == mat.col_size());
   matrix<T> ret(mat.row_size(), mat.col_size());
   ret.diag(0) = 1;
-  if ((size_t)abs(ge(ret = merge_lr(mat, ret), true)) != mat.row_size()) return {};
+  if ((u64)abs(ge(ret = merge_lr(mat, ret), true)) != mat.row_size()) return {};
   for (size_t i = 0; i < mat.row_size(); ++i) ret.data()[std::slice((i * 2 + 1) * mat.col_size(), mat.col_size(), 1)] /= std::valarray<T>(ret(i, i), mat.col_size());
   ret = ret.submat(0, mat.row_size(), mat.col_size(), mat.col_size() * 2);
   return ret;
--- src/test_cpverifier/library-checker/inverse_matrix.test.cpp
+++ src/test_cpverifier/library-checker/inverse_matrix.test.cpp
@@ -1,16 +1,21 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/inverse_matrix"
 
-#include "../../code/math/matrix.hpp"
+#include "../../code/math/mat.hpp"
+#include "../../code/math/mat_ge_basic.hpp"
+#include "../../code/math/mat_inv.hpp"
 #include "../../code/math/mint_s30.hpp"
 
 using mint = tifa_libs::math::mint_s30<998244353>;
+using mat = tifa_libs::math::matrix<mint>;
 
 int main() {
   tifa_libs::u64 n;
   std::cin >> n;
-  tifa_libs::math::matrix<mint> a(n, n, [](mint const &x) { return x == 0; });
+  mat a(n, n);
   std::cin >> a;
-  auto res = a.inverse();
+  auto is_0 = [](mint const &x) { return x == 0; };
+  auto ge = [&is_0](mat &m, bool f) { return tifa_libs::math::ge_basic(m, is_0, f); };
+  auto res = tifa_libs::math::inverse(a, ge);
   if (res)
     std::cout << res.value();
   else
