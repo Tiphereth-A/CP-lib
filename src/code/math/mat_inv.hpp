5d2669f3bafa1b52bc30d7433e927b55f9596947
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-09-26 18:30:51 +0800

    fix: name

--- src/code/math/mat_inv.hpp
+++ src/code/math/mat_inv.hpp
@@ -17,7 +17,7 @@ inline std::optional<matrix<T>> mat_inv(matrix<T> const &mat, Is0 is0, Ge ge) {
   if ((u64)abs(ge(ret = mat_merge_lr(mat, ret), true)) != n) return {};
   for (size_t i = 0; i < n; ++i)
     if (is0(ret(i, i))) return {};
-  ret.apply(0, n, n, n * 2, [&ret](size_t i, [[maybe_unused]] size_t j, T &val) { val /= ret(i, i); });
+  ret.apply_range(0, n, n, n * 2, [&ret](size_t i, [[maybe_unused]] size_t j, T &val) { val /= ret(i, i); });
   return ret.submat(0, n, n, n * 2);
 }
 
--- src/code/math/mat_trans.hpp
+++ src/code/math/mat_trans.hpp
@@ -9,7 +9,7 @@ template <class Mat, class T = typename Mat::value_type>
 inline Mat transpose(Mat const &mat) {
   size_t r_ = mat.row(), c_ = mat.col();
   Mat ret(c_, r_);
-  ret.apply(0, c_, 0, r_, [&mat](size_t i, size_t j, T &val) { val = mat(j, i); });
+  ret.apply_range(0, c_, 0, r_, [&mat](size_t i, size_t j, T &val) { val = mat(j, i); });
   return ret;
 }
 
