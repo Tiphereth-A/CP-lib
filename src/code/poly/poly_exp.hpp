1b83c301a8a657deaee1e424a23741e470237dfe
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-02 03:59:20 +0800

    fix: simplify

--- src/code/poly/poly_exp.hpp
+++ src/code/poly/poly_exp.hpp
@@ -1,35 +1,42 @@
 #ifndef TIFALIBS_POLY_EXP
 #define TIFALIBS_POLY_EXP
 
-#include "poly_dq_.hpp"
 #include "poly_ln.hpp"
 
 namespace tifa_libs::math {
 
+namespace polyexp_detail_ {
+template <class T>
+constexpr void exp_(poly<T> const &p, poly<T> &ans, size_t n) {
+  using mint = typename T::value_type;
+  if (n == 1) {
+    ans[0] = 1;
+    return;
+  }
+  if (n & 1) {
+    exp_(p, ans, n - 1);
+    n -= 2;
+    mint _ = 0;
+    for (size_t i = 0; i <= n; ++i) _ += (i + 1) * p[i + 1] * ans[n - i];
+    ans.data().push_back(_ * mint(n + 1).inv());
+    return;
+  }
+  exp_(p, ans, n / 2);
+  auto ans_ln = ans;
+  ans_ln.resize(n);
+  ans_ln = poly_ln(ans_ln);
+  for (size_t i = 0; i < ans_ln.size(); ++i) ans_ln[i] = p[i] - ans_ln[i];
+  ans_ln[0] = ans_ln[0] + 1;
+  ans.conv(ans_ln);
+  ans.resize(n);
+}
+}  // namespace polyexp_detail_
+
 template <class T>
 inline poly<T> poly_exp(poly<T> const &p) {
-  using mint = typename T::value_type;
   assert(p[0] == 0);
   poly<T> ans;
-  poly_dq_(
-    ans,
-    p.size(),
-    1,
-    [&p](poly<T> &ans, size_t n) -> void {
-      n -= 2;
-      mint _ = 0;
-      for (size_t i = 0; i <= n; ++i) _ += (i + 1) * p[i + 1] * ans[n - i];
-      ans.data().push_back(_ * mint(n + 1).inv());
-    },
-    [&p](poly<T> &ans, size_t n) -> void {
-      auto ans_ln = ans;
-      ans_ln.resize(n);
-      ans_ln = poly_ln(ans_ln);
-      for (size_t i = 0; i < ans_ln.size(); ++i) ans_ln[i] = p[i] - ans_ln[i];
-      ans_ln[0] = ans_ln[0] + 1;
-      ans.conv(ans_ln);
-      ans.resize(n);
-    });
+  polyexp_detail_::exp_(p, ans, p.size());
   return ans;
 }
 
--- src/code/poly/poly_inv.hpp
+++ src/code/poly/poly_inv.hpp
@@ -1,31 +1,39 @@
 #ifndef TIFALIBS_POLY_INV
 #define TIFALIBS_POLY_INV
 
-#include "poly_dq_.hpp"
+#include "poly.hpp"
 
 namespace tifa_libs::math {
 
+namespace polyinv_detail_ {
+template <class T>
+constexpr void inv_(poly<T> const &p, poly<T> &ans, size_t n) {
+  using mint = typename T::value_type;
+  if (n == 1) {
+    ans[0] = p[0].inv();
+    return;
+  }
+  if (n & 1) {
+    inv_(p, ans, n - 1);
+    --n;
+    mint _ = 0;
+    for (size_t i = 0; i < n; ++i) _ += ans[i] * p[n - i];
+    ans.data().push_back(_ * (-p[0]).inv());
+    return;
+  }
+  inv_(p, ans, n / 2);
+  auto sA = p;
+  sA.conv(ans);
+  sA.conv(ans);
+  sA.resize(n);
+  ans = ans * 2 - sA;
+}
+}  // namespace polyinv_detail_
+
 template <class T>
 inline poly<T> poly_inv(poly<T> const &p) {
-  using mint = typename T::value_type;
   poly<T> ans;
-  poly_dq_(
-    ans,
-    p.size(),
-    p[0].inv(),
-    [&p](poly<T> &ans, size_t n) {
-      --n;
-      mint _ = 0;
-      for (size_t i = 0; i < n; ++i) _ += ans[i] * p[n - i];
-      ans.data().push_back(_ * (-p[0]).inv());
-    },
-    [&p](poly<T> &ans, size_t n) {
-      auto sA = p;
-      sA.conv(ans);
-      sA.conv(ans);
-      sA.resize(n);
-      ans = ans * 2 - sA;
-    });
+  polyinv_detail_::inv_(p, ans, p.size());
   return ans;
 }
 
