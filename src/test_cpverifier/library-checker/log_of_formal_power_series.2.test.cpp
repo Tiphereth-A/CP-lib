66902159ff16677e540250bd08cd67f95af90b7a
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-02 02:27:43 +0800

    feat: test for poly_ln

---
+++ src/test_cpverifier/library-checker/log_of_formal_power_series.2.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/log_of_formal_power_series"
+
+#include "../../code/math/mint_ss.hpp"
+#include "../../code/poly/poly.hpp"
+#include "../../code/poly/poly_ln.hpp"
+#include "../../code/poly/polydata_s.hpp"
+
+using mint = tifa_libs::math::mint_ss<998244353>;
+using poly_t = tifa_libs::math::poly<tifa_libs::math::polydata_s<mint>>;
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  tifa_libs::u32 n;
+  std::cin >> n;
+  poly_t p(n);
+  std::cin >> p;
+  std::cout << tifa_libs::math::poly_ln(p);
+  return 0;
+}
