3f333e0bb9724826fa12e09907e1292ebe09a8a2
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-03 02:03:50 +0800

    fix: poly_pow

--- src/code/poly/poly_pow.hpp
+++ src/code/poly/poly_pow.hpp
@@ -2,6 +2,7 @@
 #define TIFALIBS_POLY_POW
 
 #include "../math/euler_phi_u64.hpp"
+#include "../math/qpow.hpp"
 #include "poly_exp.hpp"
 #include "poly_ln.hpp"
 #include "poly_shl.hpp"
@@ -15,7 +16,7 @@ inline poly<T> poly_pow(poly<T> const &p, u64 y) {
   if (y == 1) return p;
   size_t y_ = y, n = p.size();
   size_t l0 = std::find_if(p.data().begin(), p.data().end(), [](auto const &x) { return x != 0; }) - p.data().begin();
-  if (l0 > 0 && y >= n / l0) return poly<T>(n);
+  if (l0 > 0 && y > n / l0) return poly<T>(n);
   auto _ = p;
   if (y == 0) {
     std::fill(_.data().begin() + 1, _.data().end(), 0);
@@ -25,8 +26,10 @@ inline poly<T> poly_pow(poly<T> const &p, u64 y) {
   if (u64 phi = euler_phi_u64(mint::mod()); y > phi) y = y % phi + phi;
   _ = poly_shl(_, l0);
   _.strip();
-  if (_[0] != 1) _ *= _[0].inv();
+  mint _0 = _[0];
+  if (_0 != 1) _ *= _0.inv();
   _ = poly_exp(poly_ln(_) * y);
+  if (_0 != 1) _ *= qpow(_0, y);
   _.resize(n);
   poly_shr(_, l0 * y_);
   return _;
