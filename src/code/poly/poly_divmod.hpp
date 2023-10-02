6ba00dbb9012d75b3f9b88ebb3bb3a81dcaa45aa
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-03 00:06:45 +0800

    fix: strip

--- src/code/poly/poly_divmod.hpp
+++ src/code/poly/poly_divmod.hpp
@@ -9,11 +9,14 @@ template <class T>
 inline std::pair<poly<T>, poly<T>> poly_divmod(poly<T> const &p, poly<T> const &q) {
   size_t n = p.size(), m = q.size();
   if (n < m) return {poly<T>(), p};
-  auto d = poly_div(p, q), r = d;
+  auto d = poly_div(p, q);
+  d.strip();
+  auto r = d;
   r.conv(q);
   r.resize(m - 1);
   r = p - r;
   r.resize(m - 1);
+  r.strip();
   return {d, r};
 }
 
