a21486f1ef8683446ffb9746e38aa5b40615ca6a
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-03 00:29:54 +0800

    feat: test for poly_pow

--- src/code/poly/poly_pow.hpp
+++ src/code/poly/poly_pow.hpp
@@ -1,22 +1,34 @@
 #ifndef TIFALIBS_POLY_POW
 #define TIFALIBS_POLY_POW
 
+#include "../math/euler_phi_u64.hpp"
 #include "poly_exp.hpp"
 #include "poly_ln.hpp"
+#include "poly_shl.hpp"
+#include "poly_shr.hpp"
 
 namespace tifa_libs::math {
 
 template <class T>
-inline poly<T> poly_acos(poly<T> const &p, u64 y) {
+inline poly<T> poly_pow(poly<T> const &p, u64 y) {
+  using mint = typename T::value_type;
   if (y == 1) return p;
-  assert(p[0] == 1);
+  size_t y_ = y, n = p.size();
+  size_t l0 = std::find_if(p.data().begin(), p.data().end(), [](auto const &x) { return x != 0; }) - p.data().begin();
+  if (l0 > 0 && y >= n / l0) return poly<T>(n);
   auto _ = p;
   if (y == 0) {
-    std::fill(_.begin() + 1, _.end(), 0);
+    std::fill(_.data().begin() + 1, _.data().end(), 0);
+    _[0] = 1;
     return _;
   }
-  _ = poly_ln(_) * y;
-  return poly_exp(_);
+  if (u64 phi = euler_phi_u64(mint::mod()); y > phi) y = y % phi + phi;
+  _ = poly_shl(_, l0);
+  _.strip();
+  _ = poly_exp(poly_ln(_) * y);
+  _.resize(n);
+  poly_shr(_, l0 * y_);
+  return _;
 }
 
 }  // namespace tifa_libs::math
---
+++ src/test_cpverifier/library-checker/pow_of_formal_power_series.2.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/pow_of_formal_power_series"
+
+#include "../../code/math/mint_ss.hpp"
+#include "../../code/poly/poly.hpp"
+#include "../../code/poly/poly_pow.hpp"
+#include "../../code/poly/polydata_s.hpp"
+
+using mint = tifa_libs::math::mint_ss<998244353>;
+using poly_t = tifa_libs::math::poly<tifa_libs::math::polydata_s<mint>>;
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  tifa_libs::u32 n, m;
+  std::cin >> n >> m;
+  poly_t a(n);
+  std::cin >> a;
+  std::cout << tifa_libs::math::poly_pow(a, m) << '\n';
+  return 0;
+}
