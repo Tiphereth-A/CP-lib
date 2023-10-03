55edca05c037c8c771b7afc06514a39dea98d0ba
hongmao <778716966@qq.com>
2023-10-03 17:29:19 +0800

    renamed:    src/code/ds/stmin.hpp -> src/code/ds/st_0.hpp 	modified:   src/test_cpverifier/library-checker/staticrmq.0.test.cpp

--- src/test_cpverifier/library-checker/staticrmq.0.test.cpp
+++ src/test_cpverifier/library-checker/staticrmq.0.test.cpp
@@ -1,6 +1,6 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
 
-#include "../../code/ds/stmin.hpp"
+#include "../../code/ds/st_0.hpp"
 
 int main() {
   std::ios::sync_with_stdio(false);
@@ -9,7 +9,8 @@ int main() {
   std::cin >> n >> q;
   tifa_libs::vec<tifa_libs::i32> a(n);
   for (size_t i = 0; i < n; ++i) std::cin >> a[i];
-  tifa_libs::ds::sparse_table_min<tifa_libs::i32> st(n, a);
+  auto f = [](tifa_libs::i32 x, tifa_libs::i32 y) { return std::min(x, y); };
+  tifa_libs::ds::sparse_table<tifa_libs::i32, decltype(f)> st(n, a, f);
   for (size_t i = 1, u, v; i <= q; ++i) {
     std::cin >> u >> v;
     std::cout << st.query(u, v - 1) << '\n';
