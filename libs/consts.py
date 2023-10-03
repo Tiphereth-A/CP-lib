3a7d2bebdbcba6c8963a4dfe2f5d9eedc3faab49
hongmao <778716966@qq.com>
2023-10-03 13:28:27 +0800

    modified:   libs/consts.py

--- libs/consts.py
+++ libs/consts.py
@@ -10,3 +10,4 @@ CONFIG: Config = Config(os.path.join('.', 'config.yml'))
 
 CLEAN_EXT_NAME: list[str] = ['.aux', '.bbl', '.blg', '.dvi', 'fdb_latexmk', '.fls', '.log', '.nav', '.out', '.snm',
                              '.synctex.gz', '.toc', '.vrb', '.xdv']
+#123312
