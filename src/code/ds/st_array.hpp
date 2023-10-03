1d4b69a761b5f283bbb5f2a359abe9cea549f0ae
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-03 21:29:29 +0800

    fix: st_array

---
+++ src/code/ds/st_array.hpp
@@ -0,0 +1,30 @@
+#ifndef TIFA_LIBS_DS_ST_ARRAY
+#define TIFA_LIBS_DS_ST_ARRAY
+
+#include "../util/util.hpp"
+
+#include "../bit/log2.hpp"
+
+namespace tifa_libs::ds {
+
+template <class T, class F>
+class st_array {
+  vec<vec<T>> st;
+  F f;
+
+public:
+  st_array(size_t n, vec<T> const &a, F func):
+    st(n, vec<T>(size_t(bit::log2(n)) + 1)), f(func) {
+    for (size_t i = 0; i < n; ++i) st[i][0] = a[i];
+    for (size_t j = 1, m = (size_t)bit::log2(n) + 1; j < m; ++j)
+      for (size_t i = 0; i < n; ++i) st[i][j] = f(st[i][j - 1], st[std::min(n - 1, i + (1 << (j - 1)))][j - 1]);
+  }
+  T query(size_t l, size_t r) const {
+    size_t k = (size_t)bit::log2(1 + r - l);
+    return f(st[l][k], st[r - (1 << k) + 1][k]);
+  }
+};
+
+}  // namespace tifa_libs::ds
+
+#endif
--- src/test_cpverifier/library-checker/staticrmq.0.test.cpp
+++ src/test_cpverifier/library-checker/staticrmq.0.test.cpp
@@ -1,6 +1,6 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
 
-#include "../../code/ds/st.hpp"
+#include "../../code/ds/st_array.hpp"
 
 int main() {
   std::ios::sync_with_stdio(false);
@@ -10,7 +10,7 @@ int main() {
   tifa_libs::vec<tifa_libs::i32> a(n);
   for (size_t i = 0; i < n; ++i) std::cin >> a[i];
   auto f = [](tifa_libs::i32 x, tifa_libs::i32 y) { return std::min(x, y); };
-  tifa_libs::ds::sparse_table<tifa_libs::i32, decltype(f)> st(n, a, f);
+  tifa_libs::ds::st_array<tifa_libs::i32, decltype(f)> st(n, a, f);
   for (size_t i = 1, u, v; i <= q; ++i) {
     std::cin >> u >> v;
     std::cout << st.query(u, v - 1) << '\n';
