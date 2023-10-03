b91ffdcb9e8424ce7ab822d4e741053e3a2fe9cb
hongmao <778716966@qq.com>
2023-10-03 15:41:42 +0800

    feat: dsu_basic

---
+++ src/code/ds/dsu_basic.hpp
@@ -0,0 +1,26 @@
+#ifndef TIFA_LIBS_DS_DSU_BASIC
+#define TIFA_LIBS_DS_DSU_BASIC
+
+#include "../util/util.hpp"
+#include <cstddef>
+
+namespace tifa_libs::ds {
+
+class dsu_basic {
+  vec<size_t> fa;
+
+public:
+  explicit dsu_basic(size_t sz):
+    fa(sz) { std::iota(fa.begin(), fa.end(), 0); }
+
+  size_t find(size_t x) { return x == fa[x] ? fa[x] : fa[x] = find(fa[x]); }
+  void merge(size_t x, size_t y) {
+    size_t fx = find(x), fy = find(y);
+    fa[fx] = fy;
+  }
+  bool same_group(size_t x, size_t y) { return find(x) == find(y); }
+};
+
+}  // namespace tifa_libs::ds
+
+#endif
---
+++ src/test_cpverifier/library-checker/unionfind.0.test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+
+#include "../../code/ds/dsu_basic.hpp"
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  tifa_libs::u32 n, q;
+  std::cin >> n >> q;
+  tifa_libs::ds::dsu_basic dsu(n + 1);
+  for (size_t i = 1, t, u, v; i <= q; ++i) {
+    std::cin >> t >> u >> v;
+    if (t == 0) dsu.merge(u, v);
+    else std::cout << dsu.same_group(u, v) << '\n';
+  }
+  return 0;
+}
