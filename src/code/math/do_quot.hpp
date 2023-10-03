d78988dfe6f775f4ba0452b270c910f234d95758
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-03 12:43:30 +0800

    style: lint

--- src/code/math/do_quot.hpp
+++ src/code/math/do_quot.hpp
@@ -6,7 +6,7 @@
 namespace tifa_libs::math {
 
 // F: std::function<void(u64, u64, u64)>
-template<class F>
+template <class F>
 inline void do_quot(u64 n, F f) {
   for (u64 l = 1, r = 0, ed_ = n; l <= ed_; l = r + 1) f(l, r = n / (n / l), n / l);
 }
