c2f539cbf6735d729e15275842cb62761955cbd2
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-05 03:15:03 +0800

    feat: simpson & romberg

---
+++ src/code/math/romberg.hpp
@@ -0,0 +1,48 @@
+#ifndef TIFA_LIBS_MATH_ROMBERG
+#define TIFA_LIBS_MATH_ROMBERG
+
+#include "../util/util.hpp"
+
+namespace tifa_libs::math {
+
+// Func = FP(FP)
+template <class FP, class Func>
+class romberg_impl {
+private:
+  Func f;
+  FP ctqf(FP a, FP b, FP h) const {
+    FP ans = 0;
+    for (FP i = a + h * .5; i < b; i += h) ans += f(i);
+    return ans;
+  }
+
+public:
+  explicit romberg_impl(Func func):
+    f(func) {}
+  FP operator()(FP a, FP b, FP eps) const {
+    FP h = b - a;
+    FP T1 = (f(a) + f(b)) * h * .5, T2 = 0, S1 = 0, S2 = 0, C1 = 0, C2 = 0, R1 = 0, R2 = 0;
+    for (int k = 1; k < 4; h *= .5, ++k) {
+      T2 = (T1 + h * ctqf(a, b, h)) * .5;
+      S2 = (4 * T2 - T1) / 3;
+      T1 = T2;
+      if (k == 1) {
+        S1 = S2;
+        continue;
+      };
+      C2 = (16 * S2 - S1) / 15;
+      if (k == 2) {
+        C1 = C2;
+        continue;
+      };
+      R1 = (64 * C2 - C1) / 63;
+    }
+    for (;; h /= 2, T1 = T2, S1 = S2, C1 = C2, R1 = R2)
+      if (std::abs(R1 - (R2 = (64 * (C2 = (16 * (S2 = (4 * (T2 = (T1 + h * ctqf(a, b, h)) * .5) - T1) / 3) - S1) / 15) - C1) / 63)) < eps) break;
+    return R2;
+  }
+};
+
+}  // namespace tifa_libs::math
+
+#endif
---
+++ src/code/math/simpson.hpp
@@ -0,0 +1,32 @@
+#ifndef TIFA_LIBS_MATH_SIMPSON
+#define TIFA_LIBS_MATH_SIMPSON
+
+#include "../util/util.hpp"
+
+namespace tifa_libs::math {
+
+// Func = FP(FP)
+template <class FP, class Func>
+class simpson_impl {
+  Func f;
+
+  constexpr static FP simpson(FP l, FP r, FP fl, FP fmid, FP fr) { return (fl + 4 * fmid + fr) * (r - l) / 6; }
+  FP asr(FP l, FP r, FP area, FP eps, i64 dep) const {
+    FP mid = r - (r - l) * .5;
+    FP lmid = mid - (mid - l) * .5, rmid = r - (r - mid) * .5;
+    FP fl = f(l), flmid = f(lmid), fmid = f(mid), frmid = f(rmid), fr = f(r);
+    FP ls = simpson(l, mid, fl, flmid, fmid), rs = simpson(mid, r, fmid, frmid, fr);
+    if (std::abs(ls + rs - area) <= 15 * eps && dep <= 0) return ls + rs + (ls + rs - area) / 15;
+    return asr(l, mid, ls, eps * .5, dep - 1) + asr(mid, r, rs, eps * .5, dep - 1);
+  }
+
+public:
+  explicit simpson_impl(Func func):
+    f(func) {}
+  FP operator()(FP l, FP r, FP eps, i64 min_dep) const { return asr(l, r, simpson(l, r, f(l), f(r - (r - l) * .5), f(r)), eps, min_dep); }
+  FP operator()(FP l, FP r, FP eps) const { return asr(l, r, simpson(l, r, f(l), f(r - (r - l) * .5), f(r)), eps, -1); }
+};
+
+}  // namespace tifa_libs::math
+
+#endif
