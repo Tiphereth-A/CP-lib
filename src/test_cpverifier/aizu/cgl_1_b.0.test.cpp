9700f0af2ad9308085e141eb2154deb2085b7a46
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-04 21:14:52 +0800

    feat: test for refection

---
+++ src/test_cpverifier/aizu/cgl_1_b.0.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/all/CGL_1_B"
+
+#include "../../code/geo2d/line.hpp"
+#include "../../code/geo2d/point.hpp"
+#include "../../code/geo2d/proj.hpp"
+
+using point = tifa_libs::geo2d::point<double>;
+using line = tifa_libs::geo2d::line<double>;
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  line l;
+  std::cin >> l;
+  tifa_libs::u32 q;
+  std::cin >> q;
+  std::cout << std::fixed << std::setprecision(10);
+  for (size_t i = 0; i < q; ++i) {
+    point p;
+    std::cin >> p;
+    std::cout << tifa_libs::geo2d::reflect(l, p) << '\n';
+  }
+  return 0;
+}
