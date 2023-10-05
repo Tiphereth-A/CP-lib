f6dbbbc4c7a22f6dd6e0bcdd91db6b36be0ed644
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-05 18:52:11 +0800

    feat: rel_plp

---
+++ src/code/geo3d/rel_plp.hpp
@@ -0,0 +1,26 @@
+#ifndef TIFA_LIBS_GEO3D_REL_PLP
+#define TIFA_LIBS_GEO3D_REL_PLP
+
+#include "plane.hpp"
+#include "point3d.hpp"
+
+namespace tifa_libs::geo {
+
+// relation between circle and circle
+enum RELPLP {
+  above_plp,
+  in_plp,
+  below_plp
+};
+
+template <class FP>
+constexpr RELPLP relation_PlP(plane<FP> const &pl, point3d<FP> const &p) {
+  FP d = (p - pl.u) * pl.normal();
+  if (is_pos(d)) return above_plp;
+  if (is_neg(d)) return below_plp;
+  return in_plp;
+}
+
+}  // namespace tifa_libs::geo
+
+#endif
