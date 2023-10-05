ed9c399d0a0ef7f84f2ec2e2ca1606e54df0c7d6
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-06 01:32:21 +0800

    fix: simplify

--- src/code/geo2d/point.hpp
+++ src/code/geo2d/point.hpp
@@ -15,7 +15,6 @@ struct point {
 
   // s * r + t * (1 - r)
   friend constexpr point lerp(point const &s, point const &t, FP r) { return s * r + t * (1 - r); }
-
   friend constexpr point mid_point(point const &s, point const &t) { return lerp(s, t, 0.5); }
 
   constexpr point &operator+=(FP n) {
@@ -68,28 +67,21 @@ struct point {
   constexpr FP operator^(point const &p) const { return x * p.y - y * p.x; }
 
   constexpr auto arg() const { return std::atan2(y, x); }
-  friend constexpr auto arg(point const &p) { return p.arg(); }
 
   // result in [0, 2pi)
   constexpr auto arg2pi() const {
     FP _ = arg();
     return is_neg(_) ? _ + 2 * PI<FP> : _;
   }
-  // result in [0, 2pi)
-  friend constexpr auto arg2pi(point const &p) { return p.arg2pi(); }
 
   constexpr auto norm2() const { return x * x + y * y; }
-  friend constexpr auto norm2(point const &p) { return p.norm2(); }
-
   constexpr auto norm() const { return std::hypot(x, y); }
-  friend constexpr auto norm(point const &p) { return p.norm(); }
 
   constexpr static u32 QUAD__[9] = {6, 7, 8, 5, 0, 1, 4, 3, 2};
   // 4 3 2
   // 5 0 1
   // 6 7 8
   constexpr auto quad() const { return QUAD__[(sgn(y) + 1) * 3 + sgn(x) + 1]; }
-  friend constexpr auto quad(point const &p) { return p.quad(); }
 
   constexpr point &do_rot90() {
     FP tmp = x;
@@ -108,7 +100,6 @@ struct point {
   friend constexpr point rot270(point p) { return p.do_rot270(); }
 
   constexpr point &do_unit() { return *this /= norm(); }
-  friend constexpr point unit(point p) { return p.do_unit(); }
 
   constexpr point &do_rot(FP theta) {
     FP _x = x, _y = y;
@@ -116,7 +107,6 @@ struct point {
     y = _x * std::sin(theta) + _y * std::cos(theta);
     return *this;
   }
-  friend constexpr point rot(point p, FP theta) { return p.do_rot(theta); }
 };
 
 }  // namespace tifa_libs::geo
