c443102573797fb81baf8843d2e71b81119e61ba
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-05 21:22:33 +0800

    fix: tiny change

--- src/code/geo2d/argsort.hpp
+++ src/code/geo2d/argsort.hpp
@@ -8,7 +8,7 @@ namespace tifa_libs::geo {
 // sort in counterclockwise
 template <class FP>
 inline void argsort(vec<point<FP>> &vp, u32 quad_start = 6) {
-  assert(quad_start < 10);
+  assert(quad_start < 9);
   std::sort(vp.begin(), vp.end(), [ofs = 9 - quad_start](auto const &l, auto const &r) { return l.quad() == r.quad() ? is_pos(l ^ r) : (l.quad() + ofs) % 9 < (r.quad() + ofs) % 9; });
 }
 
