3d9f73f4875d0302211b7704198d740aafd47fe9
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-06 14:55:04 +0800

    feat: vvvec

--- src/code/util/util.hpp
+++ src/code/util/util.hpp
@@ -21,6 +21,8 @@ using vec = std::vector<T>;
 template <class T>
 using vvec = vec<vec<T>>;
 template <class T>
+using vvvec = vec<vvec<T>>;
+template <class T>
 using pq = std::priority_queue<T>;
 template <class T>
 using pqg = std::priority_queue<T, vec<T>, std::greater<T>>;
