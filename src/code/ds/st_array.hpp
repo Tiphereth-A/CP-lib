0bd38d06206a1ca7f6b41dd014afc9151d2e2db5
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-06 14:58:11 +0800

    fix: remove duplicate param

--- src/code/ds/st_array.hpp
+++ src/code/ds/st_array.hpp
@@ -12,7 +12,8 @@ class st_array {
   F f;
 
  public:
-  st_array(size_t n, vec<T> const &a, F func) : st(n, vec<T>(size_t(bit::log2(n)) + 1)), f(func) {
+  st_array(vec<T> const &a, F func) : st(a.size(), vec<T>(size_t(bit::log2(a.size())) + 1)), f(func) {
+    size_t n = a.size();
     for (size_t i = 0; i < n; ++i) st[i][0] = a[i];
     for (size_t j = 1, m = (size_t)bit::log2(n) + 1; j < m; ++j)
       for (size_t i = 0; i < n; ++i) st[i][j] = f(st[i][j - 1], st[std::min(n - 1, i + (1 << (j - 1)))][j - 1]);
