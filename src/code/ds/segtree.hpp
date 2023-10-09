99d967eb70c72bdedc6338239b411bb6331717a8
hongmao <hongmaoya@qq.com>
2023-10-09 17:44:36 +0800

    modified:   config.yml 	modified:   src/code/ds/segtree.hpp 	new file:   src/code/ds/weighted_segtree.hpp 	new file:   src/doc_tex/ds/weighted_segtree.tex 	new file:   src/test_tinplate/ds/weighted_segtree.cpp 	new file:   tempCodeRunnerFile.cpp

--- src/code/ds/segtree.hpp
+++ src/code/ds/segtree.hpp
@@ -41,8 +41,8 @@ class segtree {
     return ret;
   }
   T query(size_t x, size_t l, size_t r, size_t pos) { return query(x, l, r, pos, pos); }
-  explicit constexpr segtree(vec<T> const &a) : t(a.size() * 4), sign(a.size() * 4), n(a.size()) { build(a, 1, 0, n - 1); }
-  explicit constexpr segtree(std::size_t N) : t(N * 4), sign(N * 4), n(N) { build(vec<T>(n, e()), 1, 0, n - 1); }
+  explicit constexpr segtree(vec<T> const &a) : n(a.size()), t(a.size() * 4), sign(a.size() * 4) { build(a, 1, 0, n - 1); }
+  explicit constexpr segtree(std::size_t N) : n(N), t(N * 4), sign(N * 4) { build(vec<T>(n, e()), 1, 0, n - 1); }
 };
 
 }  // namespace tifa_libs::ds
