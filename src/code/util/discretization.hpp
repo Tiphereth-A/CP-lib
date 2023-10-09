7d7329ee00462493bc54ecc6e947aa33180bfc3d
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-09 23:00:35 +0800

    refactor: tiny change

--- src/code/util/discretization.hpp
+++ src/code/util/discretization.hpp
@@ -6,10 +6,15 @@
 namespace tifa_libs::util {
 
 template <class T = vec<int>>
-T discretization(T const &v) {
+T uniq(T const &v) {
   T _ = v;
   std::sort(_.begin(), _.end());
   _.erase(std::unique(_.begin(), _.end()), _.end());
+  return _;
+}
+template <class T = vec<int>>
+T gen_id(T const &v) {
+  T _ = uniq(v);
   for (auto &i : v) i = std::lower_bound(_.begin(), _.end(), i) - _.begin();
   return _;
 }
--- src/test_cpverifier/library-checker/segment_add_get_min.0.test.cpp
+++ src/test_cpverifier/library-checker/segment_add_get_min.0.test.cpp
@@ -1,6 +1,7 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/segment_add_get_min"
 
 #include "../../code/ds/lichao_segtree.hpp"
+#include "../../code/util/discretization.hpp"
 
 int main() {
   std::ios::sync_with_stdio(false);
@@ -27,18 +28,17 @@ int main() {
     else
       std::cin >> qu[i].k.l >> qu[i].k.r >> qu[i].k.a >> qu[i].k.b, qu[i].k.r -= 1, lsh.push_back(qu[i].k.l), lsh.push_back(qu[i].k.r);
   }
-  std::sort(lsh.begin(), lsh.end());
-  lsh.erase(std::unique(lsh.begin(), lsh.end()), lsh.end());
+  lsh = tifa_libs::util::uniq(lsh);
   auto _min = [](tifa_libs::i64 a, tifa_libs::i64 b) { return std::min(a, b); };
   tifa_libs::ds::lichao_segtree<tifa_libs::i64, decltype(_min)> tr(lsh, _min);
   for (size_t i = 0; i < n; ++i)
     tr.add(se[i].a, se[i].b, se[i].l, se[i].r);
   for (size_t i = 0; i < q; ++i)
     if (qu[i].opt) {
-        tifa_libs::i64 ret = tr.query(qu[i].pos);
-        if(ret == INT64_MAX) std::cout << "INFINITY\n";
-        else std::cout << ret <<'\n';
-    } else 
-    tr.add(qu[i].k.a, qu[i].k.b, qu[i].k.l, qu[i].k.r);
+      tifa_libs::i64 ret = tr.query(qu[i].pos);
+      if (ret == INT64_MAX) std::cout << "INFINITY\n";
+      else std::cout << ret << '\n';
+    } else
+      tr.add(qu[i].k.a, qu[i].k.b, qu[i].k.l, qu[i].k.r);
   return 0;
 }
