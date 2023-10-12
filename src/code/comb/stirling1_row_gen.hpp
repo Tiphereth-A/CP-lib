203001c16bc49bc6052434db0778b7d68ac00c20
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-12 23:41:22 +0800

    feat: test for stirling1_row

--- src/code/comb/stirling1_row_gen.hpp
+++ src/code/comb/stirling1_row_gen.hpp
@@ -1,44 +1,30 @@
 #ifndef TIFA_LIBS_MATH_STIRLING1_ROW_GEN
 #define TIFA_LIBS_MATH_STIRLING1_ROW_GEN
 
+#include "../bit/cntl0.hpp"
 #include "../poly/poly.hpp"
+#include "../poly/poly_shr.hpp"
 #include "../poly/poly_tsh.hpp"
 #include "fact_mod_gen.hpp"
 #include "ifact_mod_gen.hpp"
 
 namespace tifa_libs::math {
 
-namespace stirling1_row_gen_detail_ {
-
-template <class T>
-void solve(poly<T>& f, size_t n, vec<u64> const& fact, vec<u64> const& ifact) {
-  if (n == 1) {
-    f[0] = 0;
-    f[1] = 1;
-    return;
-  }
-  if (n & 1) {
-    solve(f, n - 1, fact, ifact);
-    f.resize(n + 1);
-    for (size_t i = n; i > 0; --i) (f[i] *= (n - 1)) += f[i - 1];
-    f[0] *= (n - 1);
-    return;
-  }
-  solve(f, n / 2, fact, ifact);
-  f.resize(n / 2 + 1);
-  f *= poly_tsh(f, n / 2, fact, ifact);
-  f.resize(n + 1);
-}
-
-}  // namespace stirling1_row_gen_detail_
-
 // stirling1[i] = {n \\brace i}, i=0,1,...,n
 template <class T>
 inline poly<T> stirling1_row_gen(u64 n, vec<u64> const& fact, vec<u64> const& ifact) {
   if (!n) return poly<T>{1};
-  poly<T> ans;
-  stirling1_row_gen_detail_::solve(ans, n, fact, ifact);
-  return ans;
+  poly<T> f{0, 1};
+  if (n == 1) return f;
+  for (int i = 62 - bit::cntl0(n); ~i; --i) {
+    u64 _ = n >> i;
+    f *= poly_tsh(f, _ / 2, fact, ifact);
+    f.resize(f.size() + 1);
+    if (_ & 1) f = poly_shr(f, 1) + f * (_ - 1);
+  }
+  f.resize(n + 1);
+  for (size_t i = 2; i <= n; i += 2) f[i] = -f[i];
+  return f;
 }
 // stirling1[i] = {n \\brace i}, i=0,1,...,n
 template <class T>
---
+++ src/test_cpverifier/library-checker/stirling_number_of_the_first_kind.ss.0.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/stirling_number_of_the_first_kind"
+
+#include "../../code/comb/stirling1_row_gen.hpp"
+#include "../../code/math/mint_ss.hpp"
+#include "../../code/poly/polydata_s.hpp"
+
+using mint = tifa_libs::math::mint_ss<998244353>;
+using polyd = tifa_libs::math::polydata_s<mint>;
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  tifa_libs::u32 n;
+  std::cin >> n;
+  std::cout << tifa_libs::math::stirling1_row_gen<polyd>(n);
+  return 0;
+}
