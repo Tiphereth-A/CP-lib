99781e9a54e46d5f57f96d1e5ec1152ad2c6dcda
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-12 22:48:33 +0800

    feat: bernoulli number

--- src/code/comb/bell_gen.hpp
+++ src/code/comb/bell_gen.hpp
@@ -10,15 +10,16 @@ namespace tifa_libs::math {
 
 // bell[i] = B_i, i=0,1,...,n
 template <class T>
-inline poly<T> bell_gen(u64 n) {
-  u64 mod = T::value_type::mod();
-  vec<u64> fact = fact_mod_gen(n + 1, mod), ifact = ifact_mod_gen(n + 1, mod);
+inline poly<T> bell_gen(u64 n, vec<u64> const& fact, vec<u64> const& ifact) {
   poly<T> a(n + 1);
-  for (size_t i = 0; i <= n; ++i) a[i] = ifact[i];
+  for (size_t i = 1; i <= n; ++i) a[i] = ifact[i];
   a = poly_exp(a);
-  for (size_t i = 0; i <= n; ++i) a[i] *= fact[i];
+  for (size_t i = 1; i <= n; ++i) a[i] *= fact[i];
   return a;
 }
+// bell[i] = B_i, i=0,1,...,n
+template <class T>
+inline poly<T> bell_gen(u64 n) { return bell_gen<T>(n, fact_mod_gen(n + 1, T::value_type::mod()), ifact_mod_gen(n + 1, T::value_type::mod())); }
 
 }  // namespace tifa_libs::math
 
---
+++ src/test_cpverifier/library-checker/bernoulli_number.s63.0.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/bernoulli_number"
+
+#include "../../code/comb/bernoulli_gen.hpp"
+#include "../../code/math/mint_s63.hpp"
+#include "../../code/poly/polydata_s.hpp"
+
+using mint = tifa_libs::math::mint_s63<998244353>;
+using polyd = tifa_libs::math::polydata_s<mint>;
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  tifa_libs::u32 n;
+  std::cin >> n;
+  std::cout << tifa_libs::math::bernoulli_gen<polyd>(n);
+  return 0;
+}
