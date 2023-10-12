7545d5a2b2e4b5fcdd816e271af7a290e558d7d8
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-13 00:21:49 +0800

    feat: test for stirling1_col

--- src/code/comb/stirling1_col_gen.hpp
+++ src/code/comb/stirling1_col_gen.hpp
@@ -3,26 +3,26 @@
 
 #include "../math/inverse.hpp"
 #include "../poly/poly.hpp"
-#include "../poly/poly_ln.hpp"
 #include "../poly/poly_pow.hpp"
 #include "fact_mod_gen.hpp"
+#include "inv_mod_gen.hpp"
 
 namespace tifa_libs::math {
 
 // stirling1[i] = {i \\brack k}, i=0,1,...,n
 template <class T>
-inline poly<T> stirling1_col_gen(u64 n, u64 k, vec<u64> const& fact) {
-  if (k > n) return poly<T>(n + 1);
-  u64 mod = T::value_type::mod();
+inline poly<T> stirling1_col_gen(u64 n, u64 k, vec<u64> const& fact, vec<u64> const& inv) {
+  if (n < k) return poly<T>(n + 1);
   poly<T> f(n + 1);
-  f[0] = f[1] = 1;
-  f = poly_pow(poly_ln(f), k) * inverse(fact[k], mod);
-  for (size_t i = 0; i <= n; ++i) f[i] *= ((i - k) & 1 ? mod - fact[i] : fact[i]);
+  for (size_t i = 1; i <= n; ++i) f[i] = inv[i];
+  f = poly_pow(f, k) * inverse(fact[k], T::value_type::mod());
+  for (size_t i = k; i <= n; ++i) f[i] *= fact[i];
+  for (size_t i = k; i <= n; ++i) f[i] *= ((i ^ k) & 1 ? -1 : 1);
   return f;
 }
 // stirling1[i] = {i \\brack k}, i=0,1,...,n
 template <class T>
-inline poly<T> stirling1_col_gen(u64 n, u64 k) { return stirling1_col_gen<T>(n, k, fact_mod_gen(n + 1, T::value_type::mod())); }
+inline poly<T> stirling1_col_gen(u64 n, u64 k) { return stirling1_col_gen<T>(n, k, fact_mod_gen(n + 1, T::value_type::mod()), inv_mod_gen(n + 1, T::value_type::mod())); }
 
 }  // namespace tifa_libs::math
 
---
+++ src/test_cpverifier/library-checker/stirling_number_of_the_first_kind_fixed_k.s30.0.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/stirling_number_of_the_first_kind_fixed_k"
+
+#include "../../code/comb/stirling1_col_gen.hpp"
+#include "../../code/math/mint_s30.hpp"
+#include "../../code/poly/polydata_s.hpp"
+
+using mint = tifa_libs::math::mint_s30<998244353>;
+using polyd = tifa_libs::math::polydata_s<mint>;
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  tifa_libs::u32 n, k;
+  std::cin >> n >> k;
+  auto ans = tifa_libs::math::stirling1_col_gen<polyd>(n, k);
+  for (size_t i = k; i <= n; ++i) std::cout << ans[i] << " \n"[i == n];
+  return 0;
+}
