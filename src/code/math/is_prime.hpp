4b2f0045723be95995e1ff5760d87ce02004aafc
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-13 02:40:59 +0800

    refactor: faster

--- src/code/math/is_prime.hpp
+++ src/code/math/is_prime.hpp
@@ -11,17 +11,22 @@ constexpr bool is_prime(u64 n) {
   if (n <= 2) return n == 2;
   if (~n & 1) return false;
   if (n < 8) return true;
-  constexpr u64 bases[12] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
-  u64 d = (n - 1) >> bit::cntr0(n - 1);
-  for (u64 i : bases) {
-    if (n == i) return true;
+  auto f = [d = (n - 1) >> bit::cntr0(n - 1), n](u64 i) {
     u64 t = d, y = qpow_mod(i, t, n);
     while (t != n - 1 && y != 1 && y != n - 1) {
       y = mul_mod_u(y, y, n);
       t <<= 1;
     }
-    if (y != n - 1 && (~t & 1)) return false;
-  }
+    return !(y != n - 1 && (~t & 1));
+  };
+  if (n <= (u32)-1) {
+    for (auto i : {2, 7, 61})
+      if (!f((u64)i)) return false;
+  } else
+    for (auto i : {2, 325, 9375, 28178, 450775, 9780504, 1795265022}) {
+      if (n < (u64)i) return true;
+      if (!f((u64)i)) return false;
+    }
   return true;
 }
 
