1eaf02a2588b7220e9ae787f355cdd93f0661d90
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-12 21:46:43 +0800

    feat: test for poly_tsh

--- src/code/poly/poly_tsh.hpp
+++ src/code/poly/poly_tsh.hpp
@@ -1,22 +1,31 @@
 #ifndef TIFALIBS_POLY_TSH
 #define TIFALIBS_POLY_TSH
 
+#include "../comb/fact_mod_gen.hpp"
+#include "../comb/ifact_mod_gen.hpp"
 #include "poly.hpp"
 
 namespace tifa_libs::math {
 
 template <class T>
-inline poly<T> poly_tsh(poly<T> const &f, T c, vec<u64> const &fact, vec<u64> const &ifact) {
+inline poly<T> poly_tsh(poly<T> const &f, typename T::value_type c, vec<u64> const &fact, vec<u64> const &ifact) {
   size_t n = f.size();
   poly<T> s = f, p(f.size());
   for (size_t i = 0; i < n; ++i) p[n - i - 1] = f[i] * fact[i];
   {
-    T buf = 1;
-    for (size_t i = 0; i < n; ++i, buf *= c) s[i] = buf * ifact[i];
+    typename T::value_type _ = 1;
+    for (size_t i = 0; i < n; ++i, _ *= c) s[i] = _ * ifact[i];
   }
   (p *= s).resize(n);
   for (size_t i = 0; i < n; ++i) s[n - i - 1] = p[i] * ifact[n - i - 1];
   return s;
+}
+template <class T>
+inline poly<T> poly_tsh(poly<T> const &f, typename T::value_type c) {
+  using mint = typename T::value_type;
+  size_t n = f.size();
+  vec<u64> fact = fact_mod_gen(n, mint::mod()), ifact = ifact_mod_gen(n, mint::mod());
+  return poly_tsh(f, c, fact, ifact);
 }
 
 }  // namespace tifa_libs::math
