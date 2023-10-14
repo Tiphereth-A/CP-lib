fc85797813056de2ea4e8899e1d1b7d66ac9fb08
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-15 00:28:47 +0800

    feat: polysp_pow

---
+++ src/code/poly/polysp_pow.hpp
@@ -0,0 +1,45 @@
+#ifndef TIFALIBS_POLYSP_POW
+#define TIFALIBS_POLYSP_POW
+
+#include "../comb/inv_mod_gen.hpp"
+#include "../math/qpow.hpp"
+#include "poly_shl.hpp"
+#include "poly_shr.hpp"
+#include "polysp.hpp"
+
+namespace tifa_libs::math {
+
+template <class T>
+inline poly<T> polysp_pow(poly<T> const& p, u64 y, vec<u64> const& inv, size_t n = 0) {
+  if (!n) n = p.size();
+  if (!y) return poly<T>{1}.pre(n);
+  if (p.data().empty()) return p.pre(n);
+  size_t l0 = std::find_if(p.data().begin(), p.data().end(), [](auto const& x) { return x != 0; }) - p.data().begin();
+  if ((u128)l0 * y >= n) return poly<T>(n);
+  if (l0) {
+    auto _ = poly_shr(p, l0), g = polysp_pow(_, y, inv, n - l0 * y);
+    g.resize(n);
+    return poly_shl(g, l0 * y);
+  }
+  polysp<T> ps = poly2sp(p, n);
+  poly<T> g(n);
+  g[0] = qpow(p[0], y);
+  auto _ = p[0].inv();
+  for (u64 k = 1, y_ = y % T::value_type::mod(); k < n; ++k) {
+    for (auto& [j, pj] : ps) {
+      if (k < j) break;
+      g[k] += pj * g[k - j] * ((y_ + 1) * j - k);
+    }
+    g[k] *= _ * inv[k];
+  }
+  return g;
+}
+template <class T>
+inline poly<T> polysp_pow(poly<T> const& p, u64 y, size_t n = 0) {
+  if (!n) n = p.size();
+  return polysp_pow(p, y, inv_mod_gen(n, T::value_type::mod()), n);
+}
+
+}  // namespace tifa_libs::math
+
+#endif
---
+++ src/test_cpverifier/library-checker/pow_of_formal_power_series_sparse.ss.0.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/pow_of_formal_power_series_sparse"
+
+#include "../../code/math/mint_ss.hpp"
+#include "../../code/poly/polydata_s.hpp"
+#include "../../code/poly/polysp_pow.hpp"
+
+using mint = tifa_libs::math::mint_ss<998244353>;
+using pldt_t = tifa_libs::math::polydata_s<mint>;
+using poly_t = tifa_libs::math::poly<pldt_t>;
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  tifa_libs::u32 n, k;
+  tifa_libs::u64 m;
+  std::cin >> n >> k >> m;
+  poly_t p(n);
+  for (size_t i = 0, x; i < k; ++i) std::cin >> x >> p[x];
+  std::cout << tifa_libs::math::polysp_pow(p, m);
+  return 0;
+}
