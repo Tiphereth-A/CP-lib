98695a83e1c471118864ab3c1bca55aa5ede6aee
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-15 04:25:28 +0800

    fix: kth_root

--- src/code/math/kth_root.hpp
+++ src/code/math/kth_root.hpp
@@ -1,46 +1,61 @@
 #ifndef TIFA_LIBS_MATH_KTH_ROOT
 #define TIFA_LIBS_MATH_KTH_ROOT
 
-#include "../math/bsgs.hpp"
 #include "../math/inverse.hpp"
-#include "../math/pfactors.hpp"
+#include "../math/isqrt.hpp"
 #include "../math/qpow_mod.hpp"
 
 namespace tifa_libs::math {
 
 inline std::optional<u64> kth_root(u64 a, u64 k, u64 p) {
-  if (!k) {
-    if (a != 1) return {};
-    return a;
-  }
-  if (a <= 1 || k <= 1 || p == 2) return a;
-  a %= p;
+  if (k > 0 && !(a % p)) return 0;
+  k %= p - 1;
   u64 g = std::gcd(k, p - 1);
   if (qpow_mod(a, (p - 1) / g, p) != 1) return {};
-  auto pps = pfactors(g);
-  u64 k2 = inverse(k / g, (p - 1) / g);
-  rand::Gen<std::uniform_int_distribution<u64>> rnd(2, p - 1);
-  u64 c = qpow_mod(a, k2, p);
-  for (auto [q, e] : pps) {
-    u64 s = p - 1, t = 0;
-    while (!(s % q)) {
-      s /= q;
-      ++t;
+  a = qpow_mod(a, inverse(k / g, (p - 1) / g), p);
+  auto peth_root = [](u64 a, u64 p, u32 e, u64 mod) {
+    u64 q = mod - 1;
+    u32 s = 0;
+    while (q % p == 0) {
+      q /= p;
+      ++s;
     }
-    u64 v;
-    while (1)
-      if (qpow_mod(v = rnd(), (p - 1) / q, p) != 1) break;
-    u64 qe = qpow_mod(q, e, p), u = qe - inverse(s, qe), z = qpow_mod(c, (s * u + 1) / qe, p), qi = qpow_mod(q, t - 1 - e, p);
-    for (u64 i = t - 1 - e; ~i; --i) {
-      u64 z2 = qpow_mod(v, s * qpow_mod(q, t - 1 - e - i, p), p);
-      auto L = bsgs(qpow_mod(z2, qe * qi, p), c * qpow_mod(qpow_mod(z, (p - 2) * qe, p), qi, p), q);
-      if (!L.has_value()) return {};
-      z *= qpow_mod(z2, L.value(), p);
-      qi /= q;
+    u64 pe = qpow_mod(p, e, mod), ans = qpow_mod(a, ((pe - 1) * inverse(q, pe) % pe * q + 1) / pe, mod), c = 2;
+    while (qpow_mod(c, (mod - 1) / p, mod) == 1) ++c;
+    c = qpow_mod(c, q, mod);
+    std::map<u64, u32> map;
+    u64 add = 1;
+    u32 v = isqrt((s - e) * p) + 1;
+    u64 mul = qpow_mod(c, v * qpow_mod(p, s - 1, mod - 1) % (mod - 1), mod);
+    for (u32 i = 0; i <= v; ++i) {
+      map[add] = i;
+      add = add * mul % mod;
     }
-    c = z;
+    mul = inverse(qpow_mod(c, qpow_mod(p, s - 1, mod - 1), mod), mod);
+    for (u32 i = e; i < s; ++i) {
+      u64 err = inverse(qpow_mod(ans, pe, mod), mod) * a % mod, target = qpow_mod(err, qpow_mod(p, s - 1 - i, mod - 1), mod);
+      for (u32 j = 0; j <= v; ++j) {
+        if (map.find(target) != map.end()) {
+          u32 x = map[target];
+          ans = ans * qpow_mod(c, (j + v * x) * qpow_mod(p, i - e, mod - 1) % (mod - 1), mod) % mod;
+          break;
+        }
+        target = target * mul % mod;
+        assert(j != v);
+      }
+    }
+    return ans;
+  };
+  for (u64 div = 2; div * div <= g; ++div) {
+    u32 sz = 0;
+    while (!(g % div)) {
+      g /= div;
+      ++sz;
+    }
+    if (sz) a = peth_root(a, div, sz, p);
   }
-  return c;
+  if (g > 1) a = peth_root(a, g, 1, p);
+  return a;
 }
 
 }  // namespace tifa_libs::math
