8f5eeeb48b168394f69ee41fe38f34a7af85d50c
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-15 01:57:03 +0800

    refactor: faster

--- src/code/math/is_prime.hpp
+++ src/code/math/is_prime.hpp
@@ -10,19 +10,27 @@ namespace tifa_libs::math {
 constexpr bool is_prime(u64 n) {
   if (n <= 2) return n == 2;
   if (~n & 1) return false;
-  if (n < 8) return true;
-  constexpr u64 bases[12] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
-  u64 d = (n - 1) >> bit::cntr0(n - 1);
-  for (u64 i : bases) {
-    if (n == i) return true;
-    u64 t = d, y = qpow_mod(i, t, n);
-    while (t != n - 1 && y != 1 && y != n - 1) {
-      y = mul_mod_u(y, y, n);
-      t <<= 1;
+  if (n < 8 || n == 61) return true;
+
+  auto f = [n, d = (n - 1) >> bit::cntr0(n - 1)](auto const& bases) -> bool {
+    for (u64 i : bases) {
+      if (!(i % n)) continue;
+      u64 t = d, y = qpow_mod(i, t, n);
+      while (t != n - 1 && y != 1 && y != n - 1) {
+        y = mul_mod_u(y, y, n);
+        t <<= 1;
+      }
+      if (y != n - 1 && (~t & 1)) return false;
     }
-    if (y != n - 1 && (~t & 1)) return false;
+    return true;
+  };
+
+  if (n < (1 << 30)) {
+    constexpr u64 bases[3] = {2, 7, 61};
+    return f(bases);
   }
-  return true;
+  constexpr u64 bases[7] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+  return f(bases);
 }
 
 }  // namespace tifa_libs::math
