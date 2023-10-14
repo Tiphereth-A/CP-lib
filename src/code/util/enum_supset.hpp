dd250c56f79e02683cb451eb9b98b3754fcfe6b1
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-15 05:46:12 +0800

    fix: tiny change

--- src/code/util/enum_supset.hpp
+++ src/code/util/enum_supset.hpp
@@ -8,7 +8,7 @@ namespace tifa_libs::util {
 // enumerate x : x \in {n} and x \superset b
 inline vec<u32> enum_supset(u32 b, u32 n) {
   vec<u32> res;
-  for (u32 i = b; i < (1 << n); i = (i + 1) | b) res.push_back(i);
+  for (u32 i = b; i < (1u << n); i = (i + 1) | b) res.push_back(i);
   return res;
 }
 
