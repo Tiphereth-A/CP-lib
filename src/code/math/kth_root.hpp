db1815752a57e91c7f05edddb6782ad59d952646
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-15 04:34:24 +0800

    refactor: simplify

--- src/code/math/kth_root.hpp
+++ src/code/math/kth_root.hpp
@@ -13,48 +13,45 @@ inline std::optional<u64> kth_root(u64 a, u64 k, u64 p) {
   u64 g = std::gcd(k, p - 1);
   if (qpow_mod(a, (p - 1) / g, p) != 1) return {};
   a = qpow_mod(a, inverse(k / g, (p - 1) / g), p);
-  auto peth_root = [](u64 a, u64 p, u32 e, u64 mod) {
-    u64 q = mod - 1;
+  auto pethr = [](u64 a, u64 p, u32 e, u64 m) {
+    u64 q = m - 1;
     u32 s = 0;
-    while (q % p == 0) {
+    while (!(q % p)) {
       q /= p;
       ++s;
     }
-    u64 pe = qpow_mod(p, e, mod), ans = qpow_mod(a, ((pe - 1) * inverse(q, pe) % pe * q + 1) / pe, mod), c = 2;
-    while (qpow_mod(c, (mod - 1) / p, mod) == 1) ++c;
-    c = qpow_mod(c, q, mod);
-    std::map<u64, u32> map;
-    u64 add = 1;
+    u64 pe = qpow_mod(p, e, m), ans = qpow_mod(a, (mul_mod_u(pe - 1, inverse(q, pe), pe) * q + 1) / pe, m), c = 2;
+    while (qpow_mod(c, (m - 1) / p, m) == 1) ++c;
+    c = qpow_mod(c, q, m);
+    std::map<u64, u32> mp;
+    u64 _a = 1;
     u32 v = isqrt((s - e) * p) + 1;
-    u64 mul = qpow_mod(c, v * qpow_mod(p, s - 1, mod - 1) % (mod - 1), mod);
-    for (u32 i = 0; i <= v; ++i) {
-      map[add] = i;
-      add = add * mul % mod;
-    }
-    mul = inverse(qpow_mod(c, qpow_mod(p, s - 1, mod - 1), mod), mod);
+    u64 _m = qpow_mod(c, mul_mod_u(v, qpow_mod(p, s - 1, m - 1), m - 1), m);
+    for (u32 i = 0; i <= v; ++i, _a = mul_mod_u(_a, _m, m)) mp[_a] = i;
+    _m = inverse(qpow_mod(c, qpow_mod(p, s - 1, m - 1), m), m);
     for (u32 i = e; i < s; ++i) {
-      u64 err = inverse(qpow_mod(ans, pe, mod), mod) * a % mod, target = qpow_mod(err, qpow_mod(p, s - 1 - i, mod - 1), mod);
+      u64 _ = mul_mod_u(a, inverse(qpow_mod(ans, pe, m), m), m), t = qpow_mod(_, qpow_mod(p, s - 1 - i, m - 1), m);
       for (u32 j = 0; j <= v; ++j) {
-        if (map.find(target) != map.end()) {
-          u32 x = map[target];
-          ans = ans * qpow_mod(c, (j + v * x) * qpow_mod(p, i - e, mod - 1) % (mod - 1), mod) % mod;
+        if (mp.find(t) != mp.end()) {
+          u32 x = mp[t];
+          ans = mul_mod_u(ans, qpow_mod(c, mul_mod_u(j + v * x, qpow_mod(p, i - e, m - 1), m - 1), m), m);
           break;
         }
-        target = target * mul % mod;
+        t = mul_mod_u(t, _m, m);
         assert(j != v);
       }
     }
     return ans;
   };
-  for (u64 div = 2; div * div <= g; ++div) {
-    u32 sz = 0;
-    while (!(g % div)) {
-      g /= div;
-      ++sz;
+  for (u64 d = 2; d * d <= g; ++d) {
+    u32 n = 0;
+    while (!(g % d)) {
+      g /= d;
+      ++n;
     }
-    if (sz) a = peth_root(a, div, sz, p);
+    if (n) a = pethr(a, d, n, p);
   }
-  if (g > 1) a = peth_root(a, g, 1, p);
+  if (g > 1) a = pethr(a, g, 1, p);
   return a;
 }
 
