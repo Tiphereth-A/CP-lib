9a6b1819f43c41864e3ee0ec059924fea9256e33
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-16 01:41:50 +0800

    style: simplify

--- src/code/conv/conv_3ntt.hpp
+++ src/code/conv/conv_3ntt.hpp
@@ -10,15 +10,9 @@ namespace tifa_libs::math {
 // 167772161, 469762049, 754974721
 template <class mint0, class mint1, class mint2>
 inline vec<u64> conv_3ntt(vec<u64> const &l, vec<u64> const &r, u64 mod, size_t ans_size) {
-  static constexpr u32 m0 = mint0::mod();
-  static constexpr u32 m1 = mint1::mod();
-  static constexpr u32 m2 = mint2::mod();
-  static const u32 r01 = mint1(m0).inv().val();
-  static const u32 r02 = mint2(m0).inv().val();
-  static const u32 r12 = mint2(m1).inv().val();
-  static const u32 r02r12 = (u32)mul_mod_u(r02, r12, m2);
-  static const u64 w1 = m0 % mod;
-  static const u64 w2 = mul_mod_u(m0, m1, mod);
+  static constexpr u32 m0 = mint0::mod(), m1 = mint1::mod(), m2 = mint2::mod();
+  static const u32 r01 = mint1(m0).inv().val(), r02 = mint2(m0).inv().val(), r12 = mint2(m1).inv().val(), r02r12 = (u32)mul_mod_u(r02, r12, m2);
+  static const u64 w1 = m0 % mod, w2 = mul_mod_u(m0, m1, mod);
 
   vec<mint0> d0 = conv_ntt<mint0>(l, r, ans_size);
   vec<mint1> d1 = conv_ntt<mint1>(l, r, ans_size);
