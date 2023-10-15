b12ee15e81243b2310603b6a8be2f536c6261ba5
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-15 13:35:48 +0800

    feat: new tests

---
+++ src/test_cpverifier/library-checker/bitwise_and_convolution.ds.0.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/bitwise_and_convolution"
+
+#include "../../code/conv/conv_and.hpp"
+#include "../../code/math/mint_ds.hpp"
+
+using mint = tifa_libs::math::mint_ds<-1>;
+
+int main() {
+  mint::set_mod(998244353);
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  tifa_libs::u32 n;
+  std::cin >> n;
+  tifa_libs::vec<mint> a(1 << n), b(1 << n);
+  for (auto &i : a) std::cin >> i;
+  for (auto &i : b) std::cin >> i;
+  auto c = tifa_libs::math::conv_and(a, b);
+  for (size_t i = 0; i < c.size(); ++i) std::cout << c[i] << " \n"[i + 1 == c.size()];
+  return 0;
+}
