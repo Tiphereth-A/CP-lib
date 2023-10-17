bb271a6130d976e68372bcf36302f414eae1562e
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-18 00:21:07 +0800

    fix: revert mpi(string_view) for non-little endian

--- src/code/math/mpi.hpp
+++ src/code/math/mpi.hpp
@@ -2,7 +2,9 @@
 #define TIFA_LIBS_MATH_MPI
 
 #include "../conv/conv_u128.hpp"
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 #include "../fast/str2uint_1e8.hpp"
+#endif
 #include "../util/traits.hpp"
 
 namespace tifa_libs::math {
@@ -21,6 +23,7 @@ class mpi {
       if (x < 0) neg = true, x = -x;
     while (x) dt.push_back(x % D), x /= D;
   }
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
   mpi(std::string s) : neg(false) {
     assert(!s.empty());
     if (s.size() == 1u && s[0] == '0') return;
@@ -36,6 +39,20 @@ class mpi {
       dt.push_back(str2uint_1e8(s.data() + ie - lgD));
     if (_) dt.push_back(_);
   }
+#else
+  mpi(std::string_view s) : neg(false) {
+    assert(!s.empty());
+    if (s.size() == 1u && s[0] == '0') return;
+    i32 l = 0;
+    if (s[0] == '-') ++l, neg = true;
+    for (i32 ie = (i32)s.size(); l < ie; ie -= lgD) {
+      i32 is = std::max(l, ie - (i32)lgD);
+      u64 x = 0;
+      for (i32 i = is; i < ie; ++i) x = x * 10 + (s[(size_t)i] & 15);
+      dt.push_back((u32)x);
+    }
+  }
+#endif
 
   constexpr static u32 digit() { return D; }
   constexpr static u32 log_digit() { return lgD; }
