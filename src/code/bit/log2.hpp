5b09d2c553830e14b867b8ef143e7a0992798b54
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-18 23:24:52 +0800

    refactor(log2.hpp): simplify

--- src/code/bit/log2.hpp
+++ src/code/bit/log2.hpp
@@ -1,16 +1,13 @@
 #ifndef TIFA_LIBS_BIT_LOG2
 #define TIFA_LIBS_BIT_LOG2
 
-#include "cntl0.hpp"
+#include "bwidth.hpp"
 
 namespace tifa_libs::bit {
 
 // From GCC lib
 template <typename T>
-constexpr int log2(T x) {
-  if (x == 0) return -1;
-  return (int)sizeof(T) * 8 - cntl0(x >> 1);
-}
+constexpr int log2(T x) { return bwidth(x) - 1; }
 
 }  // namespace tifa_libs::bit
 
