4063f86adf61775624e3ab79bc3bf12e130979da
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-20 00:06:36 +0800

    feat: poly_ctsh

---
+++ src/code/poly/poly_ctsh.hpp
@@ -0,0 +1,58 @@
+#ifndef TIFALIBS_POLY_CTSH
+#define TIFALIBS_POLY_CTSH
+
+#include "../comb/ifact_mod_gen.hpp"
+#include "poly.hpp"
+
+namespace tifa_libs::math {
+
+template <class T>
+inline poly<T> poly_ctsh(poly<T> const &f, typename T::value_type c, vec<u64> const &ifact, usz m = 0) {
+  usz n = f.size(), k = f.size() - 1;
+  if (!m) m = n;
+  u64 t = c.val();
+  using mint = typename T::value_type;
+  t %= mint::mod();
+  if (t <= k) {
+    poly<T> ret(m);
+    usz ptr = 0;
+    for (usz i = t; i <= k && ptr < m; ++i) ret[ptr++] = f[i];
+    if (k + 1 < t + m) {
+      auto suf = poly_ctsh(f, k + 1, ifact, m - ptr);
+      for (usz i = k + 1; i < t + m; ++i) ret[ptr++] = suf[i - (k + 1)];
+    }
+    return ret;
+  }
+  if (t + m > mint::mod()) {
+    auto pref = poly_ctsh(f, t, ifact, mint::mod() - t), suf = poly_ctsh(f, 0, ifact, m - pref.size());
+    std::copy(suf.data().begin(), suf.data().end(), std::back_inserter(pref.data()));
+    return pref;
+  }
+  poly<T> d(k + 1);
+  for (usz i = 0; i <= k; ++i) {
+    d[i] = ifact[i];
+    (d[i] *= ifact[k - i]) *= f[i];
+    if ((k - i) & 1) d[i] = -d[i];
+  }
+  poly<T> h(m + k);
+  for (usz i = 0; i < m + k; ++i) h[i] = mint(t - k + i).inv();
+  auto dh = d * h;
+  poly<T> ret(m);
+  mint cur = t;
+  for (usz i = 1; i <= k; ++i) cur *= t - i;
+  for (usz i = 0; i < m; ++i) {
+    ret[i] = cur * dh[k + i];
+    (cur *= t + i + 1) *= h[i];
+  }
+  return ret;
+}
+template <class T>
+inline poly<T> poly_ctsh(poly<T> const &f, typename T::value_type c, usz m = 0) {
+  usz n = f.size();
+  if (n == 1) return f;
+  return poly_ctsh(f, c, ifact_mod_gen(n, T::value_type::mod()), m);
+}
+
+}  // namespace tifa_libs::math
+
+#endif
---
+++ src/test_cpverifier/library-checker/shift_of_sampling_points_of_polynomial.pds32.ss.0.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/shift_of_sampling_points_of_polynomial"
+
+#include "../../code/math/mint_ss.hpp"
+#include "../../code/poly/poly.hpp"
+#include "../../code/poly/poly_ctsh.hpp"
+#include "../../code/poly/polydata_s32.hpp"
+
+using mint = tifa_libs::math::mint_ss<998244353>;
+using polyd = tifa_libs::math::polydata_s32<mint>;
+using poly_t = tifa_libs::math::poly<polyd>;
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  tifa_libs::u32 n, m, c;
+  std::cin >> n >> m >> c;
+  poly_t a(n);
+  std::cin >> a;
+  std::cout << tifa_libs::math::poly_ctsh<polyd>(a, c, m) << '\n';
+  return 0;
+}
