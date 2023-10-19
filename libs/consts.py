51e1e65944e34d93c0987a79da2f941cf092151a
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-19 20:20:09 +0800

    chore: sync TINplate

--- libs/consts.py
+++ libs/consts.py
@@ -8,5 +8,5 @@ CONTENTS_NB: str = os.path.join(CONTENTS_DIR, 'contents_notebook.tex')
 
 CONFIG: Config = Config(os.path.join('.', 'config.yml'))
 
-CLEAN_EXT_NAME: list[str] = ['.aux', '.bbl', '.blg', '.dvi', 'fdb_latexmk', '.fls', '.log', '.nav', '.out', '.snm',
-                             '.synctex.gz', '.toc', '.vrb', '.xdv']
+CLEAN_EXT_NAME: list[str] = ['.aux', '.bbl', '.blg', '.dvi', 'fdb_latexmk', '.fls',
+                             '.log', '.nav', '.out', '.pyg', '.snm', '.synctex.gz', '.toc', '.vrb', '.xdv']
