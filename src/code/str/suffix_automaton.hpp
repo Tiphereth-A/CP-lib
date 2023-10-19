c103f9fda493eb40e099ffc5ab1994f79f72e531
hongmao <hongmaoya@qq.com>
2023-10-20 01:39:44 +0800

    usage of sam

--- src/code/str/suffix_automaton.hpp
+++ src/code/str/suffix_automaton.hpp
@@ -2,7 +2,6 @@
 #define TIFA_LIBS_STR_SUFFIX_AUTOMATON
 
 #include "../util/util.hpp"
-#pragma GCC diagnostic ignored "-Wsign-conversion"
 
 namespace tifa_libs::str {
 template <u32 SZ = 26>
@@ -10,10 +9,12 @@ class suffix_automaton {
   // super root is 0
   u32 last;
   struct YYZ {
-    tifa_libs::u32 len, link;
+    u32 len, link;
     std::array<u32, SZ> nex{};
-    tifa_libs::u64 sz;
-    tifa_libs::u32 times;
+    u64 sz;         // application 1
+    u32 times;      // application 2
+    bool is_clone;  // application 3
+    u32 firstpos;   // application 3
   };
 
  public:
@@ -24,13 +25,11 @@ class suffix_automaton {
     u32 cur = sz++;
     st.push_back(YYZ());
     st[cur].len = st[last].len + 1;
-    st[cur].times = 1;
+    st[cur].times = 1;                   // application 2
+    st[cur].firstpos = st[cur].len - 1;  // application 3
     u32 p = last;
 
-    while (p != -1u && !st[p].nex[c]) {
-      st[p].nex[c] = cur;
-      p = st[p].link;
-    }
+    while (p != -1u && !st[p].nex[c]) st[p].nex[c] = cur, p = st[p].link;
     if (p == -1u) st[cur].link = 0;
     else {
       u32 q = st[p].nex[c];
@@ -41,32 +40,61 @@ class suffix_automaton {
         st[clone].nex = st[q].nex;
         st[clone].len = st[p].len + 1;
         st[clone].link = st[q].link;
+        st[clone].firstpos = st[q].firstpos;  // application 3
+        st[clone].is_clone = 1;               // application 3
 
-        while (p != -1u && st[p].nex[c] == q) {
-          st[p].nex[c] = clone;
-          p = st[p].link;
-        }
+        while (p != -1u && st[p].nex[c] == q) st[p].nex[c] = clone, p = st[p].link;
         st[q].link = st[cur].link = clone;
       }
     }
     last = cur;
   }
   vvec<u32> e;
-  void buildparenttree() {
+  void build() {
     e = vvec<u32>(sz);
     auto add = [&](u32 u, u32 v) { e[u].push_back(v); };
     for (u32 i = 1; i < sz; ++i) add(u32(st[i].link), i);
   }
-  void gettimes(u32 u = 0) {
-    for (auto v : e[u]) gettimes(v), st[u].times += st[v].times;
+
+  //!!! default: each character of t is lowercase English letters.
+  std::pair<u32, bool> search(std::string t) {  // application 0
+    u32 u = 0, i = 0, base = u32('a');
+    while (i < t.size()) {
+      if (!st[u].nex[u32(t[i]) - base]) return {i, false};
+      u = st[u].nex[u32(t[i]) - base], ++i;
+    }
+    return {u32(u), true};
   }
-  void getsz(u32 u = 0) {
+
+  void getsz(u32 u = 0) {  // application 1
     st[u].sz = 1;
     for (auto v : st[u].nex)
       if (v) {
         if (!st[v].sz) getsz(v);
         st[u].sz += st[v].sz;
       }
+  }
+
+  //!!!   need build()
+  void gettimes(u32 u = 0) {  // application 2
+    for (auto v : e[u]) gettimes(v), st[u].times += st[v].times;
+  }
+
+  //!!!   need build(), search()
+  void output_all_occurrences(u32 u, u32 P_length, vec<u32> &ans) {  // application 3
+    if (!st[u].is_clone) ans.push_back(st[u].first_pos - P_length + 1);
+    for (u32 v : e[u]) output_all_occurrences(v, P_length, ans);
+  }
+
+  //!!! default: each character of t is lowercase English letters.
+  tifa_libs::ptt<u32> lcs(std::string_view t) {  // application 4
+    u32 v = 0, len = 0, ret = 0, end = 0, base = u32('a');
+    for (u32 i = 0; i < t.size(); ++i) {
+      while (v && !st[v].nex[u32(t[i]) - base]) v = st[v].link, len = st[v].len;
+      if (st[v].nex[u32(t[i]) - base]) v = st[v].nex[u32(t[i]) - base], ++len;
+      if (len > ret) ret = len, end = i;
+    }
+    return {end - ret + 1, ret};
   }
 };
 
