2513c154a5d73065c1223c2ed9cf5e65421671f0
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-23 22:54:53 +0800

    feat: ios_container

---
+++ src/code/util/ios_container.hpp
@@ -0,0 +1,20 @@
+#ifndef TIFA_LIBS_UTIL_IOS_CONTAINER
+#define TIFA_LIBS_UTIL_IOS_CONTAINER
+
+#include "traits.hpp"
+
+template <class T, std::enable_if_t<tifa_libs::is_container<T>::value> * = nullptr>
+inline std::istream &operator>>(std::istream &is, T &x) {
+  for (auto &i : x) is >> i;
+  return is;
+}
+template <class T, std::enable_if_t<tifa_libs::is_container<T>::value> * = nullptr>
+inline std::ostream &operator<<(std::ostream &os, const T &x) {
+  if (x.begin() == x.end()) return os;
+  auto it = x.begin();
+  os << *it++;
+  for (; it != x.end(); ++it) os << ' ' << *it;
+  return os;
+}
+
+#endif
