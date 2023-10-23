fe4ae33dc3ef91b9209bf58304ef42e70622ce34
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-23 23:52:04 +0800

    fix: tiny change

--- src/code/util/simd.hpp
+++ src/code/util/simd.hpp
@@ -1,13 +1,14 @@
 #ifndef TIFA_LIBS_UTIL_SIMD
 #define TIFA_LIBS_UTIL_SIMD
 
-#pragma GCC target("avx2")
+#pragma GCC target("avx2,avx512f")
 #pragma GCC optimize("O3,unroll-loops")
 #include <immintrin.h>
 
 namespace tifa_libs {
 
 using m256 = __m256i;
+using m512 = __m512i;
 
 }  // namespace tifa_libs
 
