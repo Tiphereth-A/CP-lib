a6243b54b66c70e27ed3dc1e4579c7d7715bb2d4
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-24 02:59:04 +0800

    fix: tiny change

--- src/test_cpverifier/aizu/ntl_1_c.0.test.cpp
+++ src/test_cpverifier/aizu/ntl_1_c.0.test.cpp
@@ -7,11 +7,10 @@ constexpr u32 LIMIT = 1e7;
 
 int main() {
   u64 n;
-  tifa_libs::fin >> n;
   u32 ans;
-  tifa_libs::fin >> ans;
+  tifa_libs::fin >> n >> ans;
   tifa_libs::math::RGCD rgcd(LIMIT);
-  for (u32 i = 0, x; i < n; ++i) {
+  for (u32 i = 1, x; i < n; ++i) {
     tifa_libs::fin >> x;
     if (ans < LIMIT) (ans /= rgcd(ans, x)) *= x;
     else ans = std::lcm(ans, x);
