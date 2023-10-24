6c924ef81809dd7ce75063ecd0471e54a0234fd7
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-25 01:36:12 +0800

    feat: chrom_num

---
+++ src/code/graph/chrom_num.hpp
@@ -0,0 +1,46 @@
+#ifndef TIFA_LIBS_GRAPH_CHROM_NUM
+#define TIFA_LIBS_GRAPH_CHROM_NUM
+
+#include "../bit/cntr0.hpp"
+#include "../bit/parity.hpp"
+#include "../math/mul_mod_s.hpp"
+#include "adjlist.hpp"
+
+namespace tifa_libs::graph {
+
+namespace chrom_num_impl_ {
+
+template <u32 mod>
+u32 calc(u32 n, vec<ptt<i32>> hist) {
+  for (u32 c = 1; c <= n; ++c) {
+    i64 _ = 0;
+    for (auto& [i, x] : hist) _ += (x = (i32)math::mul_mod_s(x, i, mod));
+    if (_ % (i32)mod) return c;
+  }
+  return n;
+}
+
+}  // namespace chrom_num_impl_
+
+template <class T>
+inline u32 chrom_num(adjlist<T> const& g) {
+  u32 n = g.v_size();
+  vec<u32> adj(n), dp(1 << n);
+  for (u32 i = 0; i < n; ++i)
+    for (auto const& j : g[i]) adj[i] |= 1 << j.to, adj[j.to] |= 1 << i;
+  dp[0] = 1;
+  for (u32 i = 1; i < (1u << n); ++i) {
+    u32 k = i & (i - 1);
+    dp[i] = dp[k] + dp[k & ~adj[(u32)bit::cntr0(i)]];
+  }
+  vec<i32> _((1 << n) + 1);
+  for (u32 i = 0; i < (1u << n); ++i) _[dp[i]] += bit::parity(i) ? -1 : 1;
+  vec<ptt<i32>> hist;
+  for (u32 i = 1; i <= (1u << n); ++i)
+    if (_[i]) hist.emplace_back(i, _[i]);
+  return std::min(chrom_num_impl_::calc<1000000021>(n, hist), chrom_num_impl_::calc<1000000033>(n, hist));
+}
+
+}  // namespace tifa_libs::graph
+
+#endif
---
+++ src/test_cpverifier/library-checker/chromatic_number.0.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/chromatic_number"
+
+#include "../../code/graph/chrom_num.hpp"
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  u32 n, m;
+  std::cin >> n >> m;
+  tifa_libs::graph::adjlist<void> g(n);
+  for (u32 i = 0, u, v; i < m; ++i) {
+    std::cin >> u >> v;
+    g.add_edge(u, v);
+  }
+  std::cout << tifa_libs::graph::chrom_num(g) << '\n';
+  return 0;
+}
