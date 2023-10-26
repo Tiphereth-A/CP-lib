13c515a0d122f1bc5e2f2fe3c21d80ecef0b8e6d
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-26 23:11:49 +0800

    fix: tiny change

--- src/code/math/proot_gint.hpp
+++ src/code/math/proot_gint.hpp
@@ -11,20 +11,10 @@ template <class mint, i64 M = -1>
 inline GaussInt<mint, M> proot_gint() {
   using gint = GaussInt<mint, M>;
   const u32 m = mint::mod();
-  if (m == 65537) return {4, 17573};
   if (m == 998244353) return {1, 1};
   if (m == 999292927) return {1, 8};
-  if (m == 1000000007) return {4, 1};
+  if (m == 1000000007) return {1, 4};
   gint r = {1, 1};
-  auto next = [](gint& r) {
-    if (r.real() == 1) {
-      r.real(r.imag() + 1);
-      r.imag(1);
-      return;
-    }
-    r.real(r.real() - 1);
-    r.imag(r.imag() + 1);
-  };
   const u64 ord = (((m + 1) & 3) ? (u64)m : (u64)m * m) - 1;
   auto pf = pfactors(ord);
   while (true) {
@@ -35,7 +25,7 @@ inline GaussInt<mint, M> proot_gint() {
         break;
       }
     if (ok) break;
-    next(r);
+    r.imag(r.imag() + 1);
   }
   return r;
 }
