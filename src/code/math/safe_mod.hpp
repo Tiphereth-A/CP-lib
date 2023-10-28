02b64eed9bfd11fc8c4376db52eacadfdd10b1e3
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-29 00:57:10 +0800

    fix: tiny change

--- src/code/math/safe_mod.hpp
+++ src/code/math/safe_mod.hpp
@@ -1,12 +1,12 @@
 #ifndef TIFA_LIBS_MATH_SAFE_MOD
 #define TIFA_LIBS_MATH_SAFE_MOD
 
-#include "../util/util.hpp"
+#include "../util/traits.hpp"
 
 namespace tifa_libs::math {
 
-template <class T, std::enable_if_t<std::is_signed_v<T>, int> = 0>
-constexpr T safe_mod(T x, std::make_unsigned_t<T> mod) { return ((x %= (T)mod) < 0 ? x + (T)mod : x); }
+template <class T, std::enable_if_t<is_sint<T>::value>* = nullptr>
+constexpr T safe_mod(T x, typename to_uint<T>::type mod) { return ((x %= (T)mod) < 0 ? x + (T)mod : x); }
 
 }  // namespace tifa_libs::math
 
