bd110d654a291a7a0fcb4ec88c43933f3b7da3a2
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-28 15:09:44 +0800

    feat: upload

---
+++ src/test_cpverifier/aizu/alds1_14_a.0.test.cpp
@@ -0,0 +1,13 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/all/ALDS1_14_A"
+
+#include "../../code/str/kmp.hpp"
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::string s, p;
+  std::cin >> s >> p;
+  auto _ = tifa_libs::str::kmp(p, s);
+  for (auto i : _) std::cout << i << '\n';
+  return 0;
+}
