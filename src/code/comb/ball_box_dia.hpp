c70e206e60e24bed94a7c90d5cae3cd0571cbe9a
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-28 21:09:33 +0800

    fix: tiny change

--- src/code/comb/ball_box_dia.hpp
+++ src/code/comb/ball_box_dia.hpp
@@ -6,7 +6,7 @@
 namespace tifa_libs::math {
 
 template <class T>
-constexpr auto ball_box_dia(u64 ball, u64 box, poly<T> const& s2r) { return std::reduce(s2r.data().begin(), std::next(s2r.data().begin(), (isz)std::min(ball, box)), typename poly<T>::value_type{}); }
+constexpr auto ball_box_dia(u64 ball, u64 box, poly<T> const& s2r) { return std::reduce(s2r.data().begin(), std::next(s2r.data().begin(), (isz)std::min(ball, box) + 1), typename poly<T>::value_type{}); }
 template <class T>
 constexpr auto ball_box_dia(u64 ball, u64 box) { return ball_box_dia<T>(ball, box, stirling2_row_gen<T>(ball)); }
 
