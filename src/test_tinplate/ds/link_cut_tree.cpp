1973c0fea5757b809d7214ce3c16a9aeb2cc6b91
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-29 02:39:15 +0800

    doc: add some formula

--- src/test_tinplate/ds/link_cut_tree.cpp
+++ src/test_tinplate/ds/link_cut_tree.cpp
@@ -14,14 +14,14 @@ int main() {
   for (auto &x : a) x = 1;
   tifa_libs::ds::link_cut_tree<u64, op, inv_op> lct(n, a);
   for (u32 i = 0; i < m; ++i) {
-    char opt; 
+    char opt;
     u32 u, v;
     std::cin >> opt >> u >> v;
     --u, --v;
     if (opt == 'A') lct.link(u, v);
     else {
-        auto ret = lct.query_subtree(u, v);
-        std::cout << ret.first * ret.second << '\n';
+      auto ret = lct.query_subtree(u, v);
+      std::cout << ret.first * ret.second << '\n';
     }
   }
   return 0;
