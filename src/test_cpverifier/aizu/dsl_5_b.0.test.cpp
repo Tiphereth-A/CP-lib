63d14542ee1cf6b4d36f7cfdb3ec4ec739a8b3c2
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-29 11:18:34 +0800

    fix: tiny change

--- src/test_cpverifier/aizu/dsl_5_b.0.test.cpp
+++ src/test_cpverifier/aizu/dsl_5_b.0.test.cpp
@@ -5,16 +5,16 @@
 int main() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
+  tifa_libs::ds::fenwick2d<u32> tr(1003, 1003);
   usz n;
   std::cin >> n;
-  tifa_libs::ds::fenwick2d<u32> fw(1001, 1001);
   for (u32 i = 0, xl, xr, yl, yr; i < n; ++i) {
     std::cin >> xl >> yl >> xr >> yr;
-    fw.add(xl, yl, xr, yr, 1);
+    tr.add(xl + 1, yl + 1, xr + 1, yr + 1, 1);
   }
   u32 mx = 0;
-  for (u32 i = 0; i <= 1000; ++i)
-    for (u32 j = 0; j <= 1000; ++j) mx = std::max(mx, fw.sum(i, j));
+  for (u32 i = 1; i <= 1001; ++i)
+    for (u32 j = 1; j <= 1001; ++j) mx = std::max(mx, tr.sum(i, j));
   std::cout << mx << '\n';
   return 0;
 }
