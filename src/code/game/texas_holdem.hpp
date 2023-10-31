d45a31861459402f2d017597085c8a7b1f12d140
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-10-31 21:52:36 +0800

    fix: tiny change

--- src/code/game/texas_holdem.hpp
+++ src/code/game/texas_holdem.hpp
@@ -1,13 +1,14 @@
 #ifndef TIFA_LIBS_UTIL_TEXAS_HOLDEM
 #define TIFA_LIBS_UTIL_TEXAS_HOLDEM
 
+#include "../bit/cntlsb.hpp"
 #include "../util/util.hpp"
 
 namespace tifa_libs::game {
 
 namespace texas_holdem_impl_ {
 
-constexpr int bsr(int x) { return 31 - __builtin_clz(x); }
+constexpr int bsr(int x) { return 30 - bit::cntlsb(x); }
 // Returns the submask containing highest k bits set
 // Returns -1 instead if popcnt(a) < k
 constexpr int hbits(int a, int k) {
@@ -82,11 +83,11 @@ class th_hand {
 
  public:
   th_hand() {}
-  // Set first 5 element as hand _
-  th_hand &reset(vec<th_card> const &_) {
-    assert(_.size() >= 5);
-    cds.clear();
-    for (size_t i = 0; i < 5; ++i) cds.push_back(_[i]);
+  // Set first 5 element as hand
+  th_hand &reset(vec<th_card> const &cards) {
+    assert(cards.size() >= 5);
+    cds.resize(5);
+    std::copy(cards.begin(), cards.begin() + 5, cds.begin());
     for (auto &&card : cds) {
       auto r = card.get_rank(), s = card.get_suit();
       rka |= mps[s] |= 1 << r;
@@ -98,69 +99,76 @@ class th_hand {
   // Returns the best poker hand with the tie-breaker in [0, 2^20)
   std::pair<th_category, int> parse() const {
     assert(cds.size() == 5);
-    //! The judger of all the categories
-    const static std::function<std::tuple<bool, th_category, int>(th_hand const &)> checks[8] = {
-        // 8. STRAIGHT_FLUSH: highest (5 for A2345)
-        [](th_hand const &h) -> std::tuple<bool, th_category, int> {
-          int f = 0;
-          for (int s = 0; s < 4; ++s) f |= h.mps[s] & h.mps[s] << 1 & h.mps[s] << 2 & h.mps[s] << 3 & (h.mps[s] << 4 | h.mps[s] >> 14 << 5);
-          return {!!f, STRAIGHT_FLUSH, bsr(f)};
-        },
-        // 7. FOUR_OF_A_KIND: quadruple, other card
-        [](th_hand const &h) -> std::tuple<bool, th_category, int> {
-          if (!h.mpc[4]) return {false, FOUR_OF_A_KIND, 0};
-          const int r4 = bsr(h.mpc[4]);
-          return {true, FOUR_OF_A_KIND, r4 << 4 | bsr(h.rka ^ 1 << r4)};
-        },
-        // 6. FULL_HOUSE: triple, pair
-        [](th_hand const &h) -> std::tuple<bool, th_category, int> {
-          if (!h.mpc[3]) return {false, FULL_HOUSE, 0};
-          const int r3 = bsr(h.mpc[3]), d = (h.mpc[3] ^ 1 << r3) | h.mpc[2];
-          if (!d) return {false, FULL_HOUSE, 1};
-          const int r2 = bsr(d);
-          return {true, FULL_HOUSE, r3 << 4 | r2};
-        },
-        // 5. FLUSH: 5 highest cards
-        [](th_hand const &h) -> std::tuple<bool, th_category, int> {
-          int flush = -1;
-          for (int s = 0, _; s < 4; ++s)
-            if (flush < (_ = hbits(h.mps[s], 5))) flush = _;
-          return {flush >= 0, FLUSH, flush};
-        },
-        // 4. STRAIGHT: highest (5 for A2345)
-        [](th_hand const &h) -> std::tuple<bool, th_category, int> {
-          const int f = h.rka & h.rka << 1 & h.rka << 2 & h.rka << 3 & (h.rka << 4 | h.rka >> 14 << 5);
-          return {!!f, STRAIGHT, bsr(f)};
-        },
-        // 3. THREE_OF_A_KIND: triple, 2 highest other cards
-        [](th_hand const &h) -> std::tuple<bool, th_category, int> {
-          if (!h.mpc[3]) return {false, THREE_OF_A_KIND, 0};
-          const int r3 = bsr(h.mpc[3]);
-          return {true, THREE_OF_A_KIND, r3 << 16 | hbits(h.rka ^ 1 << r3, 2)};
-        },
-        // 2. TWO_PAIR: larger pair, smaller pair, other card
-        // 1. ONE_PAIR: pair, 3 highest other cards
-        [](th_hand const &h) -> std::tuple<bool, th_category, int> {
-          if (!h.mpc[2]) return {false, ONE_PAIR, 0};
-          const int r2 = bsr(h.mpc[2]);
-          const int d = h.mpc[2] ^ 1 << r2;
-          if (!d) return {true, ONE_PAIR, r2 << 16 | hbits(h.rka ^ 1 << r2, 3)};
-          const int r22 = bsr(d);
-          return {true, TWO_PAIR, r2 << 8 | r22 << 4 | bsr(h.rka ^ 1 << r2 ^ 1 << r22)};
-        },
-        // 0. HIGH_CARD: 5 highest cards
-        [](th_hand const &h) -> std::tuple<bool, th_category, int> { return {true, HIGH_CARD, hbits(h.rka, 5)}; }};
-    for (auto &&func : checks) {
-      auto ret = func(*this);
-      if (std::get<0>(ret)) return {std::get<1>(ret), std::get<2>(ret)};
-    }
-    //? Should be never reached
-    exit(114);
+    for (auto &&func : checks)
+      if (auto [valid, cat, ans] = func(*this); valid) return {cat, ans};
+    exit(1);
   }
   friend std::ostream &operator<<(std::ostream &os, th_hand const &hands) {
     for (size_t i = 0; i < 5 - 1; ++i) os << hands.cds[i] << ' ';
     return os << hands.cds[5 - 1];
   }
+
+ private:
+  // 8. STRAIGHT_FLUSH: highest (5 for A2345)
+  constexpr static std::tuple<bool, th_category, int> is_straight_flush(th_hand const &h) {
+    int f = 0;
+    for (int s = 0; s < 4; ++s) f |= h.mps[s] & h.mps[s] << 1 & h.mps[s] << 2 & h.mps[s] << 3 & (h.mps[s] << 4 | h.mps[s] >> 14 << 5);
+    return {!!f, STRAIGHT_FLUSH, bsr(f)};
+  }
+  // 7. FOUR_OF_A_KIND: quadruple, other card
+  constexpr static std::tuple<bool, th_category, int> is_four_of_a_kind(th_hand const &h) {
+    if (!h.mpc[4]) return {false, FOUR_OF_A_KIND, 0};
+    const int r4 = bsr(h.mpc[4]);
+    return {true, FOUR_OF_A_KIND, r4 << 4 | bsr(h.rka ^ 1 << r4)};
+  }
+  // 6. FULL_HOUSE: triple, pair
+  constexpr static std::tuple<bool, th_category, int> is_full_house(th_hand const &h) {
+    if (!h.mpc[3]) return {false, FULL_HOUSE, 0};
+    const int r3 = bsr(h.mpc[3]), d = (h.mpc[3] ^ 1 << r3) | h.mpc[2];
+    if (!d) return {false, FULL_HOUSE, 1};
+    const int r2 = bsr(d);
+    return {true, FULL_HOUSE, r3 << 4 | r2};
+  }
+  // 5. FLUSH: 5 highest cards
+  constexpr static std::tuple<bool, th_category, int> is_flush(th_hand const &h) {
+    int flush = -1;
+    for (int s = 0, _ = 0; s < 4; ++s)
+      if (flush < (_ = hbits(h.mps[s], 5))) flush = _;
+    return {flush >= 0, FLUSH, flush};
+  }
+  // 4. STRAIGHT: highest (5 for A2345)
+  constexpr static std::tuple<bool, th_category, int> is_straight(th_hand const &h) {
+    const int f = h.rka & h.rka << 1 & h.rka << 2 & h.rka << 3 & (h.rka << 4 | h.rka >> 14 << 5);
+    return {!!f, STRAIGHT, bsr(f)};
+  }
+  // 3. THREE_OF_A_KIND: triple, 2 highest other cards
+  constexpr static std::tuple<bool, th_category, int> is_three_of_a_kind(th_hand const &h) {
+    if (!h.mpc[3]) return {false, THREE_OF_A_KIND, 0};
+    const int r3 = bsr(h.mpc[3]);
+    return {true, THREE_OF_A_KIND, r3 << 16 | hbits(h.rka ^ 1 << r3, 2)};
+  }
+  // 2. TWO_PAIR: larger pair, smaller pair, other card
+  // 1. ONE_PAIR: pair, 3 highest other cards
+  constexpr static std::tuple<bool, th_category, int> is_pair(th_hand const &h) {
+    if (!h.mpc[2]) return {false, ONE_PAIR, 0};
+    const int r2 = bsr(h.mpc[2]);
+    const int d = h.mpc[2] ^ 1 << r2;
+    if (!d) return {true, ONE_PAIR, r2 << 16 | hbits(h.rka ^ 1 << r2, 3)};
+    const int r22 = bsr(d);
+    return {true, TWO_PAIR, r2 << 8 | r22 << 4 | bsr(h.rka ^ 1 << r2 ^ 1 << r22)};
+  }
+  // 0. HIGH_CARD: 5 highest cards
+  constexpr static std::tuple<bool, th_category, int> is_high_card(th_hand const &h) { return {true, HIGH_CARD, hbits(h.rka, 5)}; }
+  //! The judger of all the categories
+  constexpr static std::tuple<bool, th_category, int> (*checks[8])(th_hand const &) = {
+      is_straight_flush,
+      is_four_of_a_kind,
+      is_full_house,
+      is_flush,
+      is_straight,
+      is_three_of_a_kind,
+      is_pair,
+      is_high_card};
 };
 
 }  // namespace texas_holdem_impl_
