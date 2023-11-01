11edf377eea4459f02961091c87c66651dd5364c
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-11-02 00:09:33 +0800

    fix: tiny change

--- src/test_cpverifier/library-checker/binomial_coefficient_prime_mod.0.test.cpp
+++ src/test_cpverifier/library-checker/binomial_coefficient_prime_mod.0.test.cpp
@@ -2,20 +2,24 @@
 
 #include "../../code/comb/binom.hpp"
 #include "../../code/comb/lucas.hpp"
+#include "../../code/math/mint_ds.hpp"
 #include "../../code/util/fastio.hpp"
+
+using mint = tifa_libs::math::mint_ds<-1>;
 
 int main() {
   u32 t, m;
   tifa_libs::fin >> t >> m;
+  mint::set_mod(m);
   if (m < 1e7) {
-    tifa_libs::math::Lucas mCn(m);
+    tifa_libs::math::Lucas<mint> mCn;
     while (t--) {
       i64 n, k;
       tifa_libs::fin >> n >> k;
       tifa_libs::fout << mCn(n, k) << '\n';
     }
   } else {
-    tifa_libs::math::Binom mCn(m, 10'000'001);
+    tifa_libs::math::Binom<mint> mCn(10'000'001);
     while (t--) {
       i64 n, k;
       tifa_libs::fin >> n >> k;
--- src/test_cpverifier/library-checker/stirling_number_of_the_first_kind_small_p_large_n.0.test.cpp
+++ src/test_cpverifier/library-checker/stirling_number_of_the_first_kind_small_p_large_n.0.test.cpp
@@ -1,13 +1,17 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/stirling_number_of_the_first_kind_small_p_large_n"
 
 #include "../../code/comb/stirling1_smallp.hpp"
+#include "../../code/math/mint_ds.hpp"
+
+using mint = tifa_libs::math::mint_ds<-1>;
 
 int main() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
   u32 t, p;
   std::cin >> t >> p;
-  tifa_libs::math::Stirling1 s(p);
+  mint::set_mod(p);
+  tifa_libs::math::Stirling1<mint> s;
   while (t--) {
     u64 n, k;
     std::cin >> n >> k;
