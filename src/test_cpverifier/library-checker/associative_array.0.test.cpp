efaeaa0be870a636f2da96f5ed73096d982883d1
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-11-03 00:58:52 +0800

    feat: hmap

---
+++ src/test_cpverifier/library-checker/associative_array.0.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/associative_array"
+
+#include "../../code/util/hash_splitmix64.hpp"
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  u32 q;
+  std::cin >> q;
+  tifa_libs::hmap<u32, u32> hm;
+  for (u32 i = 0, op, k, v; i < q; ++i) {
+    std::cin >> op >> k;
+    if (op) std::cout << hm[k] << '\n';
+    else {
+      std::cin >> v;
+      hm[k] = v;
+    }
+  }
+  return 0;
+}
