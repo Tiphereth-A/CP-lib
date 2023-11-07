807a671129b59ccb0b7135a12af04f49ce93304e
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-11-08 01:35:49 +0800

    fix: tiny change

--- src/test_cpverifier/library-checker/cycle_detection_undirected.0.test.cpp
+++ src/test_cpverifier/library-checker/cycle_detection_undirected.0.test.cpp
@@ -12,7 +12,19 @@ int main() {
   tifa_libs::hmap<ptt<u32>, u32> edges;
   for (u32 i = 0, u, v; i < m; ++i) {
     std::cin >> u >> v;
-    edges[{u, v}] = edges[{v, u}] = i;
+    if (u == v) {
+      std::cout << "1\n"
+                << u << '\n'
+                << i << '\n';
+      return 0;
+    }
+    if (edges[{u, v}]) {
+      std::cout << "2\n"
+                << u << ' ' << v << '\n'
+                << i << ' ' << edges[{u, v}] - 1 << '\n';
+      return 0;
+    }
+    edges[{u, v}] = edges[{v, u}] = i + 1;
     g.add_arc(u, v);
     g.add_arc(v, u);
   }
@@ -23,6 +35,6 @@ int main() {
   }
   std::cout << res.size() << '\n';
   for (u32 i = 0; i < res.size(); ++i) std::cout << res[i].first << " \n"[i + 1 == res.size()];
-  for (u32 i = 0; i < res.size(); ++i) std::cout << edges[res[i]] << " \n"[i + 1 == res.size()];
+  for (u32 i = 0; i < res.size(); ++i) std::cout << edges[res[i]] - 1 << " \n"[i + 1 == res.size()];
   return 0;
 }
