f0a820ba9159989312d3234ea159b0614424a87b
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-11-09 16:49:37 +0800

    fix: URL

--- src/test_cpverifier/library-checker/factorial.pds32-ss.test.cpp
+++ src/test_cpverifier/library-checker/factorial.pds32-ss.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://yukicoder.me/problems/no/502"
+#define PROBLEM "https://judge.yosupo.jp/problem/factorial"
 
 #include "../../code/math/fact_mint.hpp"
 #include "../../code/math/mint_ss.hpp"
