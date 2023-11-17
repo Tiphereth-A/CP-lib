c7c743faacc0a2f9b7f4fae16501d1f59bcb4951
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-11-17 17:14:41 +0800

    Update str2uint_si64.hpp (#70)
    * Update str2uint_si64.hpp
    
    * Update strhex2uint_si64.hpp

--- src/code/fast/str2uint_si64.hpp
+++ src/code/fast/str2uint_si64.hpp
@@ -9,7 +9,7 @@ namespace tifa_libs {
 
 constexpr u32 str2uint_si64(const char* const s) {
   u64 _ = *((u64*)(s));
-  if (bit::endian::native == bit::endian::big) _ = bit::bswap(_);
+  if constexpr (bit::endian::native == bit::endian::big) _ = bit::bswap(_);
   _ = (_ & 0x0F0F0F0F0F0F0F0F) * 2561 >> 8;
   _ = (_ & 0x00FF00FF00FF00FF) * 6553601 >> 16;
   _ = (_ & 0x0000FFFF0000FFFF) * 42949672960001 >> 32;
--- src/code/fast/strhex2uint_si64.hpp
+++ src/code/fast/strhex2uint_si64.hpp
@@ -10,7 +10,7 @@ namespace tifa_libs {
 constexpr u32 strhex2uint_si64(const char* const s) {
   static_assert(__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__);
   u64 _ = *((u64*)(s));
-  if (bit::endian::native == bit::endian::big) _ = bit::bswap(_);
+  if constexpr (bit::endian::native == bit::endian::big) _ = bit::bswap(_);
   u64 _0 = _ & 0x4F4F4F4F4F4F4F4F, _1 = _0 & 0x4040404040404040;
   return u32((((((!_1 ? _0 : ((_1 >> 6) * 9) + (_0 ^ _1)) * 0x1001 >> 8) & 0x00FF00FF00FF00FF) * 0x1000001 >> 16) & 0x0000FFFF0000FFFF) * 0x1000000000001 >> 32);
 }
