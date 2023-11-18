985d91a5639c179597e2158fec50ec07f12ac2a7
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-11-19 03:44:43 +0800

    feat: berlekamp_massey

---
+++ src/code/math/berlekamp_massey.hpp
@@ -0,0 +1,39 @@
+#ifndef TIFA_LIBS_MATH_BERLEKAMP_MASSEY
+#define TIFA_LIBS_MATH_BERLEKAMP_MASSEY
+
+#include "../util/util.hpp"
+
+namespace tifa_libs::math {
+
+template <class T>
+vec<T> berlekamp_massey(vec<T> const &a) {
+  u32 n = (u32)a.size();
+  vec<T> b{1}, c{1};
+  b.reserve(n + 1);
+  c.reserve(n + 1);
+  T y = 1;
+  for (u32 k = 1; k <= n; ++k) {
+    u32 l = (u32)c.size();
+    T x = 0;
+    for (u32 i = 0; i < l; ++i) x += c[i] * a[k - l + i];
+    b.push_back(0);
+    u32 m = (u32)b.size();
+    if (x == 0) continue;
+    T d_ = x / y;
+    if (l < m) {
+      auto _ = c;
+      c.insert(c.begin(), m - l, 0);
+      for (u32 i = 0; i < m; ++i) c[m - 1 - i] -= d_ * b[m - 1 - i];
+      b = _;
+      y = x;
+    } else
+      for (u32 i = 0; i < m; ++i) c[l - 1 - i] -= d_ * b[m - 1 - i];
+  }
+  std::reverse(c.begin(), c.end());
+  for (auto &i : c) i = -i;
+  return c;
+}
+
+}  // namespace tifa_libs::math
+
+#endif
---
+++ src/test_cpverifier/library-checker/find_linear_recurrence.d31.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/find_linear_recurrence"
+
+#include "../../code/math/berlekamp_massey.hpp"
+#include "../../code/math/mint_d31.hpp"
+#include "../../code/util/ios_container.hpp"
+
+using mint = tifa_libs::math::mint_d31<-1>;
+
+int main() {
+  mint::set_mod(998244353);
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  u32 n;
+  std::cin >> n;
+  vec<mint> a(n);
+  std::cin >> a;
+  auto b = tifa_libs::math::berlekamp_massey(a);
+  std::cout << b.size() - 1 << '\n';
+  for (u32 i = 1; i < b.size(); ++i) std::cout << b[i] << " \n"[i + 1 == b.size()];
+  return 0;
+}
