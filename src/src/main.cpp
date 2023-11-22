569bf2a44a6b8c09b9b741b60c98695bcf52e0f8
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-11-22 23:17:04 +0800

    feat: add clang-format config in notebook

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -1,20 +1,14 @@
-// #pragma comment(linker, "/STACK:102400000,102400000")
 // #define _GLIBCXX_DEBUG
 #include <bits/stdc++.h>
+using namespace std;
 using i64 = int64_t;
 using u64 = uint64_t;
 using i128 = __int128_t;
 using u128 = __uint128_t;
-
 template <class Tp>
-using vec = std::vector<Tp>;
+using vec = vector<Tp>;
 template <class Tp>
-using vvec = std::vector<std::vector<Tp>>;
-template <class Tp>
-using pq = std::priority_queue<Tp>;
-template <class Tp>
-using pqg = std::priority_queue<Tp, std::vector<Tp>, std::greater<Tp>>;
-
+using vvec = vector<vector<Tp>>;
 #define for_(i, l, r, v...) for (i64 i = (l), i##e = (r), ##v; i <= i##e; ++i)
 #define fors_(i, l, r, s, v...) for (i64 i = (l), i##e = (r), ##v; i <= i##e; i += s)
 #define rfor_(i, r, l, v...) for (i64 i = (r), i##e = (l), ##v; i >= i##e; --i)
@@ -25,29 +19,25 @@ template <class... Ts>
 void dec(Ts &...x) { ((--x), ...); }
 template <class... Ts>
 void inc(Ts &...x) { ((++x), ...); }
-
 template <class T>
 void debug(T x) {
 #ifdef LOCAL_
-  std::cerr << x << std::endl;
+  cerr << x << endl;
 #endif
 }
 template <class T, class... Ts>
 void debug(T x, Ts... args) {
 #ifdef LOCAL_
-  std::cerr << x << ' ';
+  cerr << x << ' ';
   debug(args...);
 #endif
 }
-#define debug_line_ (std::cerr << __LINE__ << ' ' << __FUNCTION__ << std::endl)
+#define debug_line_ (cerr << __LINE__ << ' ' << __FUNCTION__ << endl)
 #define debug_withname_(var) debug(#var, var)
 
-using namespace std;
-
-// #define MULTI_CASES
+// #define MCAS
 void solve(int t_ = 0) {
 }
-
 signed main() {
 #ifdef LOCAL_
   auto CLOCK_ST_ = chrono::high_resolution_clock::now();
@@ -56,7 +46,7 @@ signed main() {
   cin.tie(nullptr);
   cerr << fixed << setprecision(6);
   int i_ = 0;
-#ifdef MULTI_CASES
+#ifdef MCAS
   int t_ = 0;
   cin >> t_;
   for (i_ = 0; i_ < t_; ++i_)
