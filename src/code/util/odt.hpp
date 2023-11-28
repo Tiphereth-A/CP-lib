16a7f0ef3eb83e4588bfa0c9ccf18af7b4dfafe1
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-11-29 01:31:39 +0800

    feat: add odt

---
+++ src/code/util/odt.hpp
@@ -0,0 +1,75 @@
+#ifndef TIFA_LIBS_UTIL_ODT
+#define TIFA_LIBS_UTIL_ODT
+
+#include "util.hpp"
+
+namespace tifa_libs {
+
+template <class T>
+class ODT {
+  //! [l, r]
+  struct YYZ {
+    u32 l, r;
+    mutable T v;
+    //! [l, r]
+    YYZ(u32 l, u32 r, T const &v) : l(l), r(r), v(v) {}
+    bool operator<(YYZ const &o) const { return l < o.l; }
+  };
+
+  std::set<YYZ> data;
+
+ public:
+  explicit ODT() : data() {}
+  explicit ODT(vec<T> const &c) : ODT() {
+    u32 cnt = 0;
+    for (auto &&i : c) {
+      data.emplace(cnt, cnt, i);
+      ++cnt;
+    }
+  }
+
+  void clear() { data.clear(); }
+  //! [l, r]
+  void insert(u32 l, u32 r, T const &v) { data.emplace(l, r, v); }
+  auto find(u32 x) const { return std::prev(data.upper_bound(YYZ{x, 0, 0})); }
+  auto split(u32 x) {
+    auto it = find(x);
+    if (it->l == x) return it;
+    auto [l, r, v] = *it;
+    data.erase(it), data.emplace(l, x - 1, v);
+    return data.emplace(x, r, v).first;
+  }
+  //! [l, r]
+  void assign(u32 l, u32 r, T const &v) {
+    auto itr = split(r + 1), itl = split(l);
+    data.erase(itl, itr), data.emplace(l, r, v);
+  }
+  //! [l, r]
+  // merge adjacent nodes with same value
+  void assign_merge(u32 l, u32 r, T const &v) {
+    auto itl = find(l), itr = find(r);
+    if (itr != data.end()) {
+      if (itr != std::prev(data.end()) && itr->r == r && (++itr)->v == v) r = (itr++)->r;
+      else if (itr->v != v) itr = split(r + 1), itl = find(l);
+      else r = (itr++)->r;
+    }
+    if (itl != data.begin() && itr->l == l && std::prev(itl)->v == v) l = (--itl)->l;
+    else if (itl->v != v) itl = split(l);
+    else l = itl->l;
+    data.erase(itl, itr), data.emplace(l, r, v);
+  }
+  //! [l, r]
+  // @param f: (iter) -> void
+  template <class F>
+  void run(u32 l, u32 r, F f) {
+    for (auto itr = split(r + 1), itl = split(l); itl != itr; ++itl) f(itl);
+  }
+  //! [l, r]
+  // @param f: (iter_l, iter_r) -> auto
+  template <class F>
+  auto run_no_split(u32 l, u32 r, F f) const { return f(find(l), find(r)); }
+};
+
+}  // namespace tifa_libs
+
+#endif
