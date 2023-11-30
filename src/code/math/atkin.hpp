1e7c21a236842d3fc1108a817fee45ab96cf52ea
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-12-01 01:09:23 +0800

    fix: tiny change

--- src/code/math/atkin.hpp
+++ src/code/math/atkin.hpp
@@ -19,21 +19,21 @@ inline vec<u32> atkin(u32 lim) {
   for (u32 i = 0; i < slim; ++i)
     for (u32 j = 0; j < slim; ++j) {
       if (u64 n = 4_u64 * i * i + j * j; n > lim) break;
-      else if (n % 12 == 1 || n % 12 == 5) vis[n].flip();
+      else if (u32(n) % 12 == 1 || u32(n) % 12 == 5) vis[n].flip();
     }
   for (u32 i = 0; i < slim; ++i)
     for (u32 j = 0; j < slim; ++j) {
       if (u64 n = 3_u64 * i * i + j * j; n > lim) break;
-      else if (n % 12 == 7) vis[n].flip();
+      else if (u32(n) % 12 == 7) vis[n].flip();
     }
   for (u32 i = 0; i < slim; ++i)
     for (u32 j = i - 1; ~j; --j) {
       if (u64 n = 3_u64 * i * i - j * j; n > lim) break;
-      else if (n % 12 == 11) vis[n].flip();
+      else if (u32(n) % 12 == 11) vis[n].flip();
     }
-  for (u32 i = 5; i < slim; ++i)
+  for (u32 i = 5; i <= slim; ++i)
     if (vis[i])
-      for (u32 k = i * i, j = k; j < lim; j += k) vis[j] = false;
+      for (u32 k = i * i, j = k; j <= lim; j += k) vis[j] = false;
   vec<u32> ret{2, 3};
   for (u32 x = 5; x <= lim; ++x)
     if (vis[x]) ret.push_back(x);
