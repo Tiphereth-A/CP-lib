3c9cc225534a3e0eb91157d55c8cb68158018abe
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-12-08 00:27:36 +0800

    fix: tiny change

--- src/code/tree/tree_top.hpp
+++ src/code/tree/tree_top.hpp
@@ -9,10 +9,7 @@ namespace tifa_libs::graph {
 template <class G, bool need_dfn = false, class Tinfo = std::conditional_t<std::is_base_of_v<alist, G>, tree_dfs_info, tree_dfs_info_w<typename G::weight_type>>>
 vec<u32> tree_top(G const &tr, Tinfo &info) {
   u32 n = (u32)tr.g.size();
-  if (info.maxson.empty()) {
-    if constexpr (std::is_base_of_v<alist, G>) info.template reset_dfs_info<td_maxson>(tr);
-    else info.template reset_dfs_info<td_maxson>(tr);
-  }
+  if (info.maxson.empty()) info.template reset_dfs_info<td_maxson>(tr);
   if constexpr (need_dfn) info.dfn = vec<u32>(n);
 
   vec<u32> top(n, n);
