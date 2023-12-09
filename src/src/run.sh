65340dd077e521c047fbbfcd37399fedb4e3c114
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-12-09 17:49:52 +0800

    feat: upload

---
+++ src/src/run.sh
@@ -0,0 +1,5 @@
+$bin = "bin/${@/.cpp/}"
+g++ -Wall -Wextra -o $bin "$@"
+time ./$bin < in.in > out.out & sleep 1
+[[ -n $(echo $(jobs) | grep -i Running) ]] && (echo "TLE"; killall $bin; rm $bin; exit 1)
+rm $bin
