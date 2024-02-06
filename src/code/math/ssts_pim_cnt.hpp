d79d15c3e3fcc1f2fc30fe8248d84d62ec02c1eb
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2024-02-06 19:50:54 +0800

    feat: sharp_p_subset_sum

---
+++ src/code/math/ssts_pim_cnt.hpp
@@ -0,0 +1,25 @@
+#ifndef TIFALIBS_MATH_SSTS_PIM_CNT
+#define TIFALIBS_MATH_SSTS_PIM_CNT
+
+#include "../comb/gen_inv.hpp"
+#include "../poly/poly_exp.hpp"
+
+namespace tifa_libs::math {
+
+template <class T, class U, class mint = typename T::value_type>
+poly<T> ssts_pim_cnt(vec<U> const& s, u32 t) { return ssts_pim_cnt<T, U, mint>(s, t, gen_inv<mint>(t + 1)); }
+template <class T, class U, class mint = typename T::value_type>
+poly<T> ssts_pim_cnt(vec<U> const& s, u32 t, vec<mint> const& inv) {
+  std::map<U, u32> cnt;
+  for (auto&& i : s) ++cnt[i];
+  poly<T> p(t + 1);
+  for (u32 x = 1; x <= t; ++x)
+    for (u32 d = 1; d <= t / x; ++d)
+      if (d & 1) p[d * x] += mint(cnt[x]) * inv[d];
+      else p[d * x] -= mint(cnt[x]) * inv[d];
+  return poly_exp(p);
+}
+
+}  // namespace tifa_libs::math
+
+#endif
---
+++ src/test_cpverifier/library-checker/sharp_p_subset_sum.pds-s30.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/sharp_p_subset_sum"
+
+#include "../../code/math/mint.hpp"
+#include "../../code/math/mintdata_s30.hpp"
+#include "../../code/math/ssts_pim_cnt.hpp"
+#include "../../code/poly/poly.hpp"
+#include "../../code/poly/polydata_s.hpp"
+#include "../../code/util/fastio.hpp"
+
+using mintdata = tifa_libs::math::mintdata_s30<998244353>;
+using mint = tifa_libs::math::mint<mintdata>;
+using pdata_t = tifa_libs::math::polydata_s<mint>;
+using poly_t = tifa_libs::math::poly<pdata_t>;
+
+int main() {
+  u32 n, t;
+  tifa_libs::fin >> n >> t;
+  vec<u32> s(n);
+  tifa_libs::fin >> s;
+  auto v = tifa_libs::math::ssts_pim_cnt<pdata_t>(s, t).data();
+  for (u32 i = 1; i <= t; ++i) tifa_libs::fout.write(v[i]).space_if(i != t);
+  tifa_libs::fout.linebreak();
+  return 0;
+}
