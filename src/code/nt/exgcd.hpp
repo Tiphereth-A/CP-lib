12ba8af162644a203e862eb69c444ab7be8b1e0f
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2024-04-15 23:24:22 +0800

    fix: avoid signed overflow

--- src/code/nt/exgcd.hpp
+++ src/code/nt/exgcd.hpp
@@ -9,8 +9,10 @@ namespace tifa_libs::math {
 template <uint_c U>
 constexpr auto exgcd(U a, U b) {
   using T = to_sint_t<U>;
-  if (!a) return std::make_tuple(b, (T)0, (T)!!b);
+  if (!a) return std::make_tuple(b, (T)0, (T) !!b);
   if (!b) return std::make_tuple(a, (T)1, (T)0);
+  bool f = 0;
+  if (a > b) std::swap(a, b), f = 1;
   auto r = std::__countr_zero(a | b);
   a >>= r, b >>= r;
   T x = (T)a, y = (T)b;
@@ -19,16 +21,17 @@ constexpr auto exgcd(U a, U b) {
     while (!(x & 1)) {
       x /= 2;
       if (!((s | t) & 1)) s /= 2, t /= 2;
-      else s = (s + (T)b) / 2, t = (t - (T)a) / 2;
+      else s = std::midpoint(s, (T)b), t = std::midpoint(t, -(T)a);
     }
     while (!(y & 1)) {
       y /= 2;
       if (!((u | v) & 1)) u /= 2, v /= 2;
-      else u = (u + (T)b) / 2, v = (v - (T)a) / 2;
+      else u = std::midpoint(u, (T)b), v = std::midpoint(v, -(T)a);
     }
     if (x >= y) x -= y, s -= u, t -= v;
     else y -= x, u -= s, v -= t;
   }
+  if (f) std::swap(a, b), std::swap(u, v), std::swap(s, t);
   if (y > 1) a /= (U)y, b /= (U)y;
   if (a && (U)abs(v) >= a) {
     T _ = v / (T)a;
@@ -46,7 +49,7 @@ constexpr auto exgcd(U a, U b) {
 template <sint_c T>
 constexpr auto exgcd(T a, T b) {
   using U = to_uint_t<T>;
-  if (auto [x, y] = std::minmax(a, b); x >= 0 && y <= T(U(-1) / 8)) return exgcd((U)a, (U)b);
+  if (auto [x, y] = std::minmax(a, b); x >= 0 && y <= T(U(-1) / 16)) return exgcd((U)a, (U)b);
   T s = 1, t = 0, u = 0, v = 1;
   while (b) {
     T c = a / b;
