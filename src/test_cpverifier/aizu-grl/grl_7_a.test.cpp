22551f3467e55576f3db27644b8436892ab34ff3
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2024-05-16 15:50:17 +0800

    feat: test

---
+++ src/test_cpverifier/aizu-grl/grl_7_a.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/all/GRL_7_A/"
+
+#include "../../code/graph/dinic.hpp"
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  u32 l, r, m;
+  std::cin >> l >> r >> m;
+  u32 s = l + r, t = l + r + 1;
+  tifa_libs::graph::dinic big(l + r + 2);
+  flt_ (u32, i, 0, l) big.add(s, i, 1);
+  flt_ (u32, i, 0, r) big.add(i + l, t, 1);
+  for (u32 i = 0, u, v; i < m; ++i) std::cin >> u >> v, big.add(u, v + l, 1);
+  std::cout << big(s, t) << '\n';
+  return 0;
+}
