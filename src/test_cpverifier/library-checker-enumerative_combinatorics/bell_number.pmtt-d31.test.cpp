1bcaf077958f7011ab079401de586cb2792fc016
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2024-11-14 07:12:02 +0800

    feat: test

---
+++ src/test_cpverifier/library-checker-enumerative_combinatorics/bell_number.pmtt-d31.test.cpp
@@ -0,0 +1,22 @@
+#define AUTO_GENERATED
+#define PROBLEM "https://judge.yosupo.jp/problem/bell_number"
+
+#include "../../code/comb/gen_bell.hpp"
+#include "../../code/io/fastio.hpp"
+
+CEXP u32 MOD = 998244353;
+
+#include "../../code/math/mint_d31.hpp"
+#include "../../code/poly/polymtt.hpp"
+
+using mint = tifa_libs::math::mint_d31<-1>;
+using poly = tifa_libs::math::polymtt<mint>;
+
+int main() {
+  mint::set_mod(MOD);
+  u32 n;
+  tifa_libs::fin >> n;
+  auto ans = tifa_libs::math::gen_bell<poly>(n);
+  fle_ (u32, i, 0, n) tifa_libs::fout << ans[i] << " \n"[i == n];
+  return 0;
+}
