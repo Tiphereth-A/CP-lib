21aebaa4710295d98e3fe2eea020712a79ba4887
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2025-02-20 21:27:52 +0800

    ci(verify): merge verify-normal and verify-huge

--- src/test_cpverifier/library-checker-convolution/convolution_mod_large.mints-s30.conv_large-ntt_large.test.cpp
+++ src/test_cpverifier/library-checker-convolution/convolution_mod_large.mints-s30.conv_large-ntt_large.test.cpp
@@ -11,14 +11,18 @@ CEXP u32 MOD = 998244353;
 #include "../../code/math/mint_s30.hpp"
 
 using mint = tifa_libs::math::mint<tifa_libs::math::mint_s30, MOD>;
-using ntt_t = tifa_libs::math::NTT<mint>;
+using ntt_ntt_large_t = tifa_libs::math::NTT<mint>;
+using vec_t = vec<mint>;
+auto conv_func(vec_t CR a, vec_t CR b) {
+  ntt_ntt_large_t ntt;
+  return tifa_libs::math::conv_ntt_large(ntt, a, b);
+}
 
 int main() {
   u32 n, m;
   tifa_libs::fin >> n >> m;
   vec<mint> a(n), b(m);
   tifa_libs::fin >> a >> b;
-  ntt_t ntt;
-  tifa_libs::fout << tifa_libs::math::conv_ntt_large(ntt, a, b) << '\n';
+  tifa_libs::fout << conv_func(a, b) << '\n';
   return 0;
 }
