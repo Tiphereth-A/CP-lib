635729bb4878ea249f870d510d6b35e7b33f23f3
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2025-04-19 18:34:26 +0800

    refactor(dsu_weighted): support non-commutative group

--- include/ds/dsu_weighted.hpp
+++ include/ds/dsu_weighted.hpp
@@ -5,31 +5,40 @@
 
 namespace tifa_libs::ds {
 
+//! T should be a group with op and inv
+template <class T = i64, auto op = std::plus<>{}, auto inv = std::negate<>{}>
+requires requires(T x, T y) {
+  { op(x, y) } -> std::same_as<T>;
+  { inv(x) } -> std::same_as<T>;
+}
 class dsu_weighted {
+  const T E;
   veci p;
-  vecii dep;
+  vec<T> val;  // $a_{root}^{-1}a_i$
 
  public:
-  CEXPE dsu_weighted(u32 n) NE : p(n, -1), dep(n) {}
+  CEXPE dsu_weighted(u32 n, cT_(T) E = T{}) NE : E{E}, p(n, -1), val(n, E) {}
 
   CEXP i32 find(u32 x) NE {
     if (p[x] < 0) return (i32)x;
     const auto _ = find((u32)p[x]);
-    dep[x] += dep[(u32)p[x]], p[x] = _;
+    val[x] = op(val[(u32)p[x]], val[x]), p[x] = _;
     return _;
   }
   CEXP u32 size() CNE { return (u32)p.size(); }
   CEXP u32 size(u32 x) NE { return (u32)-p[(u32)find(x)]; }
-  CEXP i64 depth(u32 x) NE {
+  CEXP T CR weight(u32 x) NE {
     find(x);
-    return dep[x];
+    return val[x];
   }
+  // $a_y^{-1}a_x$
+  CEXP T diff(u32 x, u32 y) NE { return op(inv(weight(y)), weight(x)); }
   CEXP bool same(u32 x, u32 y) NE { return find(x) == find(y); }
-  CEXP bool merge(u32 x, u32 y, i64 d = 1) NE {
-    (d += depth(y) - depth(x)) *= -1;
-    if ((x = (u32)find(x)) == (y = (u32)find(y))) return false;
-    if (p[x] > p[y]) swap(x, y), d *= -1;
-    p[x] += p[y], p[y] = (i32)x, dep[y] = d;
+  CEXP bool merge(u32 x, u32 y, T w) NE {
+    w = op(weight(x), inv(op(weight(y), w)));
+    if ((x = (u32)find(x)) == (y = (u32)find(y))) return w == E;
+    if (p[x] > p[y]) swap(x, y), w = inv(w);
+    p[x] += p[y], p[y] = (i32)x, val[y] = w;
     return true;
   }
 };
---
+++ test_cpverifier/library-checker-datastructure/unionfind_with_potential.mints-s30.test.cpp
@@ -0,0 +1,31 @@
+#define AUTO_GENERATED
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind_with_potential"
+
+#include "../../include/ds/dsu_weighted.hpp"
+
+CEXP u32 MOD = 998244353;
+
+#include "../../include/math/mint.hpp"
+#include "../../include/math/mint_s30.hpp"
+
+using mint = tifa_libs::math::mint<tifa_libs::math::mint_s30, MOD>;
+
+int main() {
+  std::cin.tie(nullptr)->std::ios::sync_with_stdio(false);
+  u32 n, q;
+  std::cin >> n >> q;
+  tifa_libs::ds::dsu_weighted<mint> dsu(n);
+  flt_ (u32, i, 0, q) {
+    u32 op, u, v;
+    std::cin >> op >> u >> v;
+    if (op) {
+      if (!dsu.same(u, v)) std::cout << "-1\n";
+      else std::cout << dsu.diff(u, v) << '\n';
+    } else {
+      i64 k;
+      std::cin >> k;
+      std::cout << dsu.merge(u, v, k) << '\n';
+    }
+  }
+  return 0;
+}
