63f95bf9b3b64b75e8592e5848869273cd6e0b3c
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2026-05-24 16:55:50 +0800

    feat(cmd.fmt): check version before formatting

--- libs/cmd/fmt.py
+++ libs/cmd/fmt.py
@@ -1,25 +1,49 @@
 import os
+import re
 import shlex
 from collections.abc import Iterable
 from pathlib import Path
 
 import click
+from packaging.version import Version
 
 from libs.content.section import _EXT_TYPE
 from libs.decorator import with_logger, with_timer
 from libs.util.run_command import run_command
 
-TYPE_LINT_COMMAND_MP: dict[str, str] = {
-    'cpp': 'clang-format --Wno-error=unknown -style=file -i {src}',
+
+class FmtConfig:
+    def __init__(self, lint_command_template: str, min_version: Version, version_command: str, version_regex: re.Pattern):
+        self.lint_command_template = lint_command_template
+        self.min_version = min_version
+        self.version_command = version_command
+        self.version_regex = version_regex
+
+
+# code type -> (lint command template, minimum version, version command, version regex)
+TYPE_LINT_COMMAND_MP: dict[str, FmtConfig] = {
+    'cpp': FmtConfig(
+        'clang-format --Wno-error=unknown -style=file -i {src}',
+        Version('22.0.0'),
+        'clang-format --version',
+        re.compile(r'^.*? clang-format version (?P<version>\d+(?:\.\d+)*) .*$')),
     # tip: use `perl -MCPAN -e shell` then `isntall YAML::Tiny`, `install File::HomeDir` to install the dependency of latexindent
-    'tex': 'latexindent -c=/ -l=.latexindent.yaml -s {src} -o {src}',
-    'py': 'autopep8 -i {src}'
+    'tex': FmtConfig(
+        'latexindent -c=/ -l=.latexindent.yaml -s {src} -o {src}',
+        Version('3.24.7'),
+        'latexindent --version',
+        re.compile(r'^(?P<version>\d+(?:\.\d+)*), .*$')),
+    'py': FmtConfig(
+        'autopep8 -i {src}',
+        Version('2.3.2'),
+        'autopep8 --version',
+        re.compile(r'^autopep8 (?P<version>\d+(?:\.\d+)*) .*$'))
 }
 
 
 @with_logger
 @with_timer
-def lint_all_codes(files: Iterable[str], jobs: int, **kwargs):
+def lint_all_codes(files: Iterable[str], jobs: int, force_lf: bool, **kwargs):
     logger = kwargs.get('logger')
     logger.info('formatting codes...')
 
@@ -38,15 +62,30 @@ def lint_all_codes(files: Iterable[str], jobs: int, **kwargs):
 
     for code_type, type_files in type_to_files.items():
         logger.info(f"formatting {code_type} files...")
+        conf: FmtConfig = TYPE_LINT_COMMAND_MP[code_type]
+        try:
+            current_version = Version(re.match(conf.version_regex, os.popen(
+                conf.version_command).read()).group('version'))
+            if current_version < conf.min_version:
+                logger.warning(
+                    f"{code_type} formatter version ({current_version}) is less than the required version ({conf.min_version}), skipping...")
+                continue
+        except Exception as e:
+            logger.error(
+                f"failed to get version for {code_type} formatter: {e}")
+            logger.debug(
+                f"version command output: '{os.popen(conf.version_command).read()}'")
+            raise e
+
         failed = run_command(
             lambda x: (
-                shlex.split(TYPE_LINT_COMMAND_MP[code_type].format(
+                shlex.split(conf.lint_command_template.format(
                     src=shlex.quote(x))),
                 {'post_run': (lambda _: Path(x).write_bytes(
                     Path(x).read_bytes().replace(
                         b'\r\n', b'\n').replace(b'\r', b'\n')
                 ))}
-                if code_type == 'tex'
+                if force_lf
                 else {},
             ),
             type_files,
@@ -63,5 +102,6 @@ def _register_fmt(cli):
     @cli.command('fmt')
     @click.argument('files', nargs=-1, type=click.Path(exists=True, dir_okay=False, file_okay=True, readable=True))
     @click.option('-j', '--jobs', type=int, help='limit of jobs to run', default=8)
-    def _format(files: tuple[str], jobs: int):
-        lint_all_codes(files, jobs)
+    @click.option('--force-lf', is_flag=True, default=True, help='force line feed (LF) as line ending')
+    def _format(files: tuple[str], jobs: int, force_lf: bool):
+        lint_all_codes(files, jobs, force_lf)
